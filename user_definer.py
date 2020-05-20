bb5a6fdd89707525cc12b0b7f2c08aa23bfb26cf
htbui <htbui@sfu.ca>
2020-05-19 21:07:48 -0700

    test multiple dimension

--- user_definer.py
+++ user_definer.py
@@ -4,6 +4,7 @@ from Shapes.ShapesFunctions import *
 
 # Specify the  file that includes dynamic systems
 from dynamics.Humannoid6D_sys1 import *
+from dynamics.DubinsCar4D import *
 import scipy.io as sio
 
 import math
@@ -19,7 +20,7 @@ import math
 """
 
 # Grid field in this order: min_range, max_range, number of dims, grid dimensions, list of periodic dim: starting at 0
-g = grid(np.array([-0.5, -1.0, 0.5, -2.0, -math.pi/2, -8.0]), np.array([0.5, 1.0, 1.5, 2.0, math.pi/2, 8.0]), 6, np.array([27, 26, 27, 26, 27, 26]))
+"""g = grid(np.array([-0.5, -1.0, 0.5, -2.0, -math.pi/2, -8.0]), np.array([0.5, 1.0, 1.5, 2.0, math.pi/2, 8.0]), 6, np.array([27, 26, 27, 26, 27, 26]))
 
 # Define my object
 my_car = Humanoid_6D()
@@ -36,6 +37,26 @@ print("Welcome to optimized_dp \n")
 
 # Use the following variable to specify the characteristics of computation
 compMethod = "minVWithVInit"
+my_object  = my_car
+my_shape = Initial_value_f """
+
+g = grid(np.array([-5.0, -5.0, -1.0, -math.pi]), np.array([5.0, 5.0, 1.0, math.pi]), 4, np.array([40, 40, 50, 50]), [3])
+
+# Define my object
+my_car = DubinsCar4D()
+
+# Use the grid to initialize initial value function
+Initial_value_f = CylinderShape(g, [3,4], np.zeros(4), 1)
+
+# Look-back lenght and time step
+lookback_length = 2.0
+t_step = 0.05
+
+tau = np.arange(start = 0, stop = lookback_length + t_step, step = t_step)
+print("Welcome to optimized_dp \n")
+
+# Use the following variable to specify the characteristics of computation
+compMethod = "none"
 my_object  = my_car
 my_shape = Initial_value_f
 
