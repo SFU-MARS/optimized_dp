6d813c1fb933e44b276d23e5d84b91a243ee9fce
Minh Bui <htbui@sfu.ca>
2020-07-24 12:09:26 -0700

    add my_atan function

--- helper/helper_math.py
+++ helper/helper_math.py
@@ -4,10 +4,30 @@ import time
 from user_definer import *
 # Custom function
 
-def my_arctan(a):
-    result = hcl.scalar(0, "result", dtype=hcl.Float())
-    result.v = np.arctan(a)
-    return result.v
+def my_atan(x):
+    my_st_result = hcl.scalar(0, "my_st_result")
+    with hcl.if_(x <= 1):
+        with hcl.if_(x >= -1):
+            my_st_result[0] = x - x*x*x/3 + x*x*x*x*x/5 - x*x*x*x*x*x*x/7 + x*x*x*x*x*x*x*x*x/9 \
+                             + x*x*x*x*x*x*x*x*x*x*x/11 - x*x*x*x*x*x*x*x*x*x*x*x*x/13 \
+                        + x*x*x*x*x*x*x*x*x*x*x*x*x*x*x/15 - x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x/17 \
+            + x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x/19 - x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x/21\
+            + x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x/23 - x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x/25
+
+        with hcl.elif_(x < -1):
+            my_st_result[0] = math.pi/2 -(1/x - 1/(x*x*x*3) + 1/(x*x*x*x*x*5) - 1/(x*x*x*x*x*x*x*7) + 1/(x*x*x*x*x*x*x*x*x*9) \
+                             + 1/(x*x*x*x*x*x*x*x*x*x*x*11) - 1/(x*x*x*x*x*x*x*x*x*x*x*x*x*13) \
+                        + 1/(x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*15) - 1/(x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*17) \
+            + 1/(x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*19) - 1/(x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*21)\
+            + 1/(x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*23) - 1/(x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*25))
+    with hcl.if_(x > 1):
+            my_st_result[0] = -math.pi/2 -(1/x - 1/(x*x*x*3) + 1/(x*x*x*x*x*5) - 1/(x*x*x*x*x*x*x*7) + 1/(x*x*x*x*x*x*x*x*x*9) \
+                             + 1/(x*x*x*x*x*x*x*x*x*x*x*11) - 1/(x*x*x*x*x*x*x*x*x*x*x*x*x*13) \
+                        + 1/(x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*15) - 1/(x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*17) \
+            + 1/(x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*19) - 1/(x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*21)\
+            + 1/(x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*23) - 1/(x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*25))
+
+    return my_st_result[0]
 
 # Input is a value, output is value
 def my_abs(my_x):
