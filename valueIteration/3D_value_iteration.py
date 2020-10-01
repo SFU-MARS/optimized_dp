ef45e82b43c6ad4e602807c760228eedab408991
georgedgiovani <georgedgiovani.dev@gmail.com>
2020-09-30 17:26:37 -0700

    Edit: hcl syntax changes
    Signed-off-by: georgedgiovani <georgedgiovani.dev@gmail.com>

--- valueIteration/3D_value_iteration.py
+++ valueIteration/3D_value_iteration.py
@@ -2,73 +2,66 @@ import heterocl as hcl
 from Grid.GridProcessing import Grid
 import numpy as np
 import time
+from user_definer import *
 
-
-# Initialize the value function as a zeros array
-# dimensions: number of indeces along each axis of the stateSpace
-def initialize(dimensions):
-    with hcl.Stage("Value Iteration"):
-        Vinit = np.zeros(dimensions)
-        return hcl.asarray(Vinit)
 
 
 # Perform value iteration to solve for the optimal value function, Vopt
 # grid:       a Grid object characterizing the state space
 # actions:    the set of actions a that can be taken from any state 
-#             TODO: extend for state-specific actions
-# rwd:        the transition reward function
+#             TODO: must be in tensor format
+# rwd:        reward for being in state s and taking action a
+#             TODO: must be in tensor format
 # trans:      given state s, action a, return the set of successor states s' and
 #             the associated probability p of reaching s'
-# gamma:      discount factor
-# epsilon:    a small number
-def valueIteration3D(grid, actions, rwd, trans, gamma, epsilon):
-    Vopt = initialize(grid.pts_each_dim)
-    Vopt = sweep(Vopt, grid, actions, rwd, trans, gamma, epsilon)
+#             TODO: how to represent the transition function as a tensor?
+# discount:   discount factor
+# epsilon:    a small number to guage congergence
+def value_iteration_3D(): 
+    Vopt = hcl.placeholder(tuple(g.pts_each_dim), name="Vopt", dtype=hcl.Float())
+    discount = hcl.scalar(0, "gamma")
+    epsilon  = hcl.scalar(0, "epsilon")
+    reSweep = hcl.scalar(0, "reSweep")
+
+    def solve_Vopt(Vinit, actions, rwd, trans, gamma, convergence_number):
+        Vopt = hcl.asarray(Vinit)
+        discount = gamma  
+        epsilon  = convergence_number
+
+        # Calculate the value of Vopt at point s given action a
+        # TODO: Is it possible to make calls to an actions(s,a) or trans(s,a) func?
+        def updateV(s):
+            return max(rwd(s, a) + 
+                discount*sum(p*Vopt[index_of_state(sNew)] for p, sNew in trans(s,a)) for a in actions)
+
+        # Return the index values associated with the given stateValues
+        # TODO: Find better way of obtaining the index from a set of state vals
+        def index_of_state(stateVals, grid):
+            i = round(stateVals[0] * grid.pts_each_dim[0] / (grid.max[0] - grid.min[0]))
+            j = round(stateVals[1] * grid.pts_each_dim[1] / (grid.max[1] - grid.min[1]))
+            k = round(stateVals[2] * grid.pts_each_dim[2] / (grid.max[2] - grid.min[2]))
+            return (i, j, k)
+
+        with hcl.Stage("Sweep_1"):
+            reSweep = 1
+            with hcl.while_(reSweep == 1):
+                reSweep = 0
+                with hcl.for_(0, g.pts_each_dim[0], name="i") as i:
+                    with hcl.for_(0, g.pts_each_dim[1], name="j") as j:
+                        with hcl.for_(0, g.pts_each_dim[2], name="k") as k:
+                            s = hcl.placeholder((1,3), "state")
+                            oldV = hcl.scalar(0, "oldV")
+                            newV = hcl.scalar(0, "newV")
+                            # Calculate the new optimal value
+                            s = (g.vs[0][(i,0,0)], g.vs[1][(0,j,0)], g.vs[2][(0,0,k)]) 
+                            oldV = Vopt[i, j, k]
+                            newV = updateV(s)
+                            Vopt[i, j, k] = newV[0]
+                            
+                            # Evaluate convergence
+                            with hcl.if_(hcl.or_((newV - oldV) > epsilon, (oldV - newV) > epsilon)):
+                                reSweep = 1
     return Vopt
 
 
-# Perform value iteration by sweeping along each possible diagonal
-# TODO: implement all possible diagonal sweeps
-def sweep(Vopt, grid, actions, rwd, trans, gamma, epsilon):
-    while (sweepOne(Vopt, grid, actions, rwd, trans, gamma)): continue
-
-
-# Sweep direction 1
-def sweepOne(Vopt, grid, actions, rwd, trans, gamma, epsilon):
-    reSweep    = hcl.scalar(0, "reSweep")
-    reSweep[0] = 0
-    with hcl.for_(0, grid.pts_each_dim[0], name="i") as i:
-        with hcl.for_(0, grid.pts_each_dim[1], name="j") as j:
-            with hcl.for_(0, grid.pts_each_dim[2], name="k") as k:
-                s    = hcl.scalar((1,3), "state")
-                oldV = hcl.scalar(0, "oldV")
-                newV = hcl.scalar(0, "newV")
-                # Calculate the new optimal value
-                s[:]    = (grid.vs[0][(i,0,0)], grid.vs[1][(0,j,0)], grid.vs[2][(0,0,k)]) 
-                oldV[0] = Vopt[i, j, k]
-                newV[0] = updateV(Vopt, s, a, actions, rwd, trans, gamma)
-                Vopt[i, j, k] = newV[0]
-                with hcl.if_(abs(newV[0] - oldV[0]) > epsilon): reSweep[0] = 1
-    return reSweep
-
-
-# Calculate the value of Vopt at point s given action a
-def updateV(Vopt, s, a, actions, rwd, trans, gamma):
-    return max(rwd(s, a) + 
-        (gamma * sum(p * Vopt(indexGivenState(sNew)) for p, sNew in trans(s, a))) for a in actions) 
-
-
-# Return the state values associated with the integer indeces of Vopt
-def calcStateValues(coordinate, grid):
-    Si = (coordinate[0] / grid.pts_each_dim[0]) * (grid.max[0] - grid.min[0])
-    Sj = (coordinate[1] / grid.pts_each_dim[1]) * (grid.max[1] - grid.min[1])
-    Sk = (coordinate[2] / grid.pts_each_dim[2]) * (grid.max[2] - grid.min[2])
-    return (Si, Sj, Sk)
-
-
-# Return the index values associated with the given stateValues
-def indexGivenState(stateVals, grid):
-    i = round(stateVals[0] * grid.pts_each_dim[0] / (grid.max[0] - grid.min[0]))
-    j = round(stateVals[1] * grid.pts_each_dim[1] / (grid.max[1] - grid.min[1]))
-    k = round(stateVals[2] * grid.pts_each_dim[2] / (grid.max[2] - grid.min[2]))
-    return (i, j, k)
+s = hcl.create_schedule([Vinit, actions, rwd, trans, gamma, convergence_number], solve_Vopt)
