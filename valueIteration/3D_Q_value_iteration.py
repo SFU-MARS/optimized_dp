a35384e6bc1ee656e689629695eec3c9b3a59af9
George Giovanis <georgedgiovani.dev@gmail.com>
2020-10-22 12:08:36 -0700

    Edit: modify 3D Q-value Iteration to make use of transition(s,a) function
    Signed-off-by: George Giovanis <georgedgiovani.dev@gmail.com>

--- valueIteration/3D_Q_value_iteration.py
+++ valueIteration/3D_Q_value_iteration.py
@@ -8,17 +8,37 @@ import time
 
 ###################################### USER-DEFINED FUNCTIONS ######################################
 
-# Return the reward for taking action from state
-def reward(i, j, k, action):
-    # s' = (ia, ja, ka)
+# Given state and action, return successor states and their probabilities
+def transition(i, j, k, action, trans):
     ia = i + action[0]
     ja = j + action[1]
     ka = k + action[2]
+    trans[0, 0] = 0.9
+    trans[1, 0] = 0.1
+    trans[0, 1] = ia
+    trans[0, 2] = ja
+    trans[0, 3] = ka
+    trans[1, 1] = i
+    trans[1, 2] = j
+    trans[1, 3] = k
+
+# Return the reward for taking action from state
+def reward(i, j, k, Qopt, action, trans):
     rwd = hcl.scalar(0, "rwd")
-    with hcl.if_(hcl.and_(ia == 8, ja == 8, ka == 8)):
-        rwd[0] = 100
-    with hcl.else_():
-        rwd[0] = 1
+    # for each possible successor
+    with hcl.for_(0, trans.shape[0], name="si") as si:
+        p  = trans[si,0]
+        ia = trans[si,1]
+        ja = trans[si,2]
+        ka = trans[si,3]
+        # check if action was legal
+        with hcl.if_(hcl.and_(ia < Qopt.shape[0], ja < Qopt.shape[1], ka < Qopt.shape[2])):
+            # add reward for reaching goal
+            with hcl.if_(hcl.and_(ia == 8, ja == 8, ka == 8)):
+                rwd[0] += (p * 100)
+            # add default reward
+            with hcl.else_():
+                rwd[0] += 1
 
     return rwd[0]
 
@@ -27,27 +47,28 @@ def reward(i, j, k, action):
 ######################################### HELPER FUNCTIONS #########################################
 
 # Update the value function at position (i,j,k)
-# NOTE: Determine if HeteroCL is pass by ref or val
-def updateQopt(i, j, k, a, Qopt, actions, gamma):
-    # s' = (ia, ja, ka)
-    action = actions[a]
-    ia = i + action[0]
-    ja = j + action[1]
-    ka = k + action[2]
+def updateQopt(i, j, k, a, Qopt, actions, trans, gamma):
+    # load successors
+    transition(i, j, k, actions[a], trans)
 
-    # check if action was legal
-    with hcl.if_(hcl.and_(ia < Qopt.shape[0], ja < Qopt.shape[1], ka < Qopt.shape[2])):
+    # initialize Qopt[i,j,k,a] with the immediate reward
+    r = hcl.scalar(0, "r")
+    r[0] = reward(i, j, k, Qopt, actions[a], trans)
+    Qopt[i,j,k,a] = r[0]
 
-        # initialize Qopt[i,j,k,a] with the immediate reward
-        r = hcl.scalar(0, "r")
-        r[0] = reward(i, j, k, action)
-        Qopt[i,j,k,a] = r[0]
+    # maximize over successor Q-values
+    with hcl.for_(0, trans.shape[0], name="si") as si:
+        p    = trans[si,0]
+        ia   = trans[si,1]
+        ja   = trans[si,2]
+        ka   = trans[si,3]
 
-        # maximize over Q(s', a')
-        # i.e. Compare the Q values at the successor state and choose the largest to use as estimate
-        with hcl.for_(0, actions.shape[0], name="a_") as a_:
-            with hcl.if_(r + (gamma[0] * Qopt[ia,ja,ka,a_]) > Qopt[i,j,k,a]):
-                Qopt[i,j,k,a] = r + (gamma[0] * Qopt[ia,ja,ka,a_])
+        # check if successor is within the grid
+        with hcl.if_(hcl.and_(ia < Qopt.shape[0], ja < Qopt.shape[1], ka < Qopt.shape[2])):
+            with hcl.for_(0, actions.shape[0], name="a_") as a_:
+                with hcl.if_(r + (gamma[0] * (p * Qopt[ia,ja,ka,a_])) > Qopt[i,j,k,a]):
+                    Qopt[i,j,k,a] = r + (gamma[0] * (p * Qopt[ia,ja,ka,a_]))
+            r = Qopt[i,j,k,a] 
 
         return Qopt[i,j,k,a]
 
@@ -65,10 +86,8 @@ def evaluateConvergence(newQ, oldQ, epsilon, reSweep):
 
 ######################################### VALUE ITERATION ##########################################
 
-# Minh: All functions defined within solve_Vopt needs to be re-written as heteroCL (hcl.while, etc)
-# Minh: Also arrays values used and passed into solve_Vopt function needs to be placeholder type
 def value_iteration_3D():
-    def solve_Qopt(Qopt, actions, gamma, epsilon, count):
+    def solve_Qopt(Qopt, actions, trans, gamma, epsilon, count):
             reSweep = hcl.scalar(1, "reSweep")
             oldQ    = hcl.scalar(0, "oldV")
             newQ    = hcl.scalar(0, "newV")
@@ -84,7 +103,7 @@ def value_iteration_3D():
                                 # For all actions
                                 with hcl.for_(0, Qopt.shape[3], name="a") as a:
                                     oldQ[0] = Qopt[i,j,k,a]
-                                    updateQopt(i, j, k, a, Qopt, actions, gamma)
+                                    updateQopt(i, j, k, a, Qopt, actions, trans, gamma)
                                     newQ[0] = Qopt[i,j,k,a]
                                     evaluateConvergence(newQ, oldQ, epsilon, reSweep)
                     count[0] += 1
@@ -102,7 +121,7 @@ def value_iteration_3D():
                                 # For all actions
                                 with hcl.for_(0, Qopt.shape[3], name="a") as a:
                                     oldQ[0] = Qopt[i2,j2,k2,a]
-                                    updateQopt(i2, j2, k2, a, Qopt, actions, gamma)
+                                    updateQopt(i2, j2, k2, a, Qopt, actions, trans, gamma)
                                     newQ[0] = Qopt[i2,j2,k2,a]
                                     evaluateConvergence(newQ, oldQ, epsilon, reSweep)
                     count[0] += 1
@@ -118,7 +137,7 @@ def value_iteration_3D():
                                 # For all actions
                                 with hcl.for_(0, Qopt.shape[3], name="a") as a:
                                     oldQ[0] = Qopt[i2,j,k,a]
-                                    updateQopt(i2, j, k, a, Qopt, actions, gamma)
+                                    updateQopt(i2, j, k, a, Qopt, actions, trans, gamma)
                                     newQ[0] = Qopt[i2,j,k,a]
                                     evaluateConvergence(newQ, oldQ, epsilon, reSweep)
                     count[0] += 1
@@ -134,7 +153,7 @@ def value_iteration_3D():
                                 # For all actions
                                 with hcl.for_(0, Qopt.shape[3], name="a") as a:
                                     oldQ[0] = Qopt[i,j2,k,a]
-                                    updateQopt(i, j2, k, a, Qopt, actions, gamma)
+                                    updateQopt(i, j2, k, a, Qopt, actions, trans, gamma)
                                     newQ[0] = Qopt[i,j2,k,a]
                                     evaluateConvergence(newQ, oldQ, epsilon, reSweep)
                     count[0] += 1
@@ -150,7 +169,7 @@ def value_iteration_3D():
                                 # For all actions
                                 with hcl.for_(0, Qopt.shape[3], name="a") as a:
                                     oldQ[0] = Qopt[i,j,k2,a]
-                                    updateQopt(i, j, k2, a, Qopt, actions, gamma)
+                                    updateQopt(i, j, k2, a, Qopt, actions, trans, gamma)
                                     newQ[0] = Qopt[i,j,k2,a]
                                     evaluateConvergence(newQ, oldQ, epsilon, reSweep)
                     count[0] += 1
@@ -167,7 +186,7 @@ def value_iteration_3D():
                                 # For all actions
                                 with hcl.for_(0, Qopt.shape[3], name="a") as a:
                                     oldQ[0] = Qopt[i2,j2,k,a]
-                                    updateQopt(i2, j2, k, a, Qopt, actions, gamma)
+                                    updateQopt(i2, j2, k, a, Qopt, actions, trans, gamma)
                                     newQ[0] = Qopt[i2,j2,k,a]
                                     evaluateConvergence(newQ, oldQ, epsilon, reSweep)
                     count[0] += 1
@@ -184,7 +203,7 @@ def value_iteration_3D():
                                 # For all actions
                                 with hcl.for_(0, Qopt.shape[3], name="a") as a:
                                     oldQ[0] = Qopt[i2,j,k2,a]
-                                    updateQopt(i2, j, k2, a, Qopt, actions, gamma)
+                                    updateQopt(i2, j, k2, a, Qopt, actions, trans, gamma)
                                     newQ[0] = Qopt[i2,j,k2,a]
                                     evaluateConvergence(newQ, oldQ, epsilon, reSweep)
                     count[0] += 1
@@ -201,7 +220,7 @@ def value_iteration_3D():
                                 # For all actions
                                 with hcl.for_(0, Qopt.shape[3], name="a") as a:
                                     oldQ[0] = Qopt[i,j2,k2,a]
-                                    updateQopt(i, j2, k2, a, Qopt, actions, gamma)
+                                    updateQopt(i, j2, k2, a, Qopt, actions, trans, gamma)
                                     newQ[0] = Qopt[i,j2,k2,a]
                                     evaluateConvergence(newQ, oldQ, epsilon, reSweep)
                     count[0] += 1
@@ -225,9 +244,10 @@ def value_iteration_3D():
     count     = hcl.placeholder((0,), "count")
     epsilon   = hcl.placeholder((0,), "epsilon")
     actions   = hcl.placeholder(tuple([3, 3]), name="actions", dtype=hcl.Float())
+    trans     = hcl.placeholder(tuple([2,4]), name="successors", dtype=hcl.Float())
 
     # Create a static schedule -- graph
-    s = hcl.create_schedule([Qopt, actions, gamma, epsilon, count], solve_Qopt)
+    s = hcl.create_schedule([Qopt, actions, trans, gamma, epsilon, count], solve_Qopt)
     
 
 
@@ -235,6 +255,7 @@ def value_iteration_3D():
 
     # Convert the python array to hcl type array
     Q_opt = hcl.asarray(np.zeros([10, 10, 10, 3]))
+    trans = hcl.asarray(np.zeros([2, 4]))
 
     # set the actions
     actions    = np.ones([3, 3])
@@ -288,7 +309,7 @@ def value_iteration_3D():
 
     # Now use the executable
     t_s = time.time()
-    f(Q_opt, actions, gamma, epsilon, count)
+    f(Q_opt, actions, trans, gamma, epsilon, count)
     t_e = time.time()
 
     Q = Q_opt.asnumpy()
