c395375ac89d3a9b5429a5e9ecf5aaa72a10bc5a
Michael Lu <lu.michael@outlook.com>
2021-02-11 01:39:17 -0800

    add way to get the value from a state
    - update grid documentation

--- Grid/GridProcessing.py
+++ Grid/GridProcessing.py
@@ -1,27 +1,82 @@
 import numpy as np
 
+
 class Grid:
-  def __init__(self, min, max, dims ,pts_each_dim, pDim=[]):
+    def __init__(self, min, max, dims, pts_each_dim, pDim=[]):
+        """ 
+
+        Args:
+            min (list): The lower bounds of each dimension in the grid
+            max (list): The upper bounds of each dimension in the grid
+            dims (int): The dimension of grid
+            pts_each_dim (list): The number of points for each dimension in the grid
+            pDim (list, optional): A list of periodic dimentions (0-indexed). Defaults to [].
+        """
         self.max = max
         self.min = min
         self.dims = len(pts_each_dim)
         self.pts_each_dim = pts_each_dim
         self.pDim = pDim
 
-        # Make some modifications to the initialized
+        # Exclude the upper bounds for periodic dimensions is not included 
+        # e.g. [-pi, pi)
         for dim in pDim:
-            self.max[dim] = self.min[dim] + (self.max[dim] - self.min[dim]) * (1 - 1/self.pts_each_dim[dim])
+            self.max[dim] = self.min[dim] + \
+                (self.max[dim] - self.min[dim]) * \
+                (1 - 1/self.pts_each_dim[dim])
         self.dx = (self.max - self.min) / (self.pts_each_dim - 1.0)
 
-        print(self.dx)
         """
         Below is re-shaping the self.vs so that we can make use of broadcasting
         self.vs[i] is reshape into (1,1, ... , pts_each_dim[i], ..., 1) such that pts_each_dim[i] is used in ith position
         """
         self.vs = []
-        for i in range(0,dims):
-            tmp = np.linspace(self.min[i],self.max[i], num=self.pts_each_dim[i])
+        self.grid_points = []
+        for i in range(0, dims):
+            tmp = np.linspace(self.min[i], self.max[i],
+                              num=self.pts_each_dim[i])
             broadcast_map = np.ones(self.dims, dtype=int)
             broadcast_map[i] = self.pts_each_dim[i]
+            self.grid_points.append(tmp)
+
+            # in order to add our range of points to our grid
+            # we need to modify the shape of tmp in order to match
+            # the size of the grid for one of the axis
             tmp = np.reshape(tmp, tuple(broadcast_map))
             self.vs.append(tmp)
+
+    def get_index(self, state):
+        """ Returns a tuple of the closest index of each state in the grid
+
+        Args:
+            state (tuple): state of dynamic object
+        """
+        index = []
+
+        for i, s in enumerate(state):
+            idx = np.searchsorted(self.grid_points[i], s)
+            if idx > 0 and (
+                idx == len(self.grid_points[i])
+                or math.fabs(s - self.grid_points[i][idx - 1])
+                < math.fabs(s - self.grid_points[i][idx])
+            ):
+                index.append(idx - 1)
+            else:
+                index.append(idx)
+
+        return tuple(index)
+
+    def get_value(self, V, state):
+        """Obtain the approximate value of a state
+
+        Assumes that the state is within the bounds of the grid
+
+        Args:
+            V (np.array): value function of solved HJ PDE 
+            state (tuple): state of dynamic object
+
+        Returns:
+            [float]: V(state)
+        """
+        index = self.get_index(state)
+        return V[index]
