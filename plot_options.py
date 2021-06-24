b0757a5b25cdc1a81adb0b45f7d9b58e89124205
Michael Lu <lu.michael@outlook.com>
2021-06-24 15:26:10 -0700

    reformat

--- plot_options.py
+++ plot_options.py
@@ -1,19 +1,16 @@
-
 class PlotOptions:
-  def __init__(self, plot_type="3d_plot", dims_plot=[], slices=[]):
-    if plot_type not in ["2d_plot", "3d_plot"]:
-        raise Exception("Illegal plot type !")
+    def __init__(self, plot_type="3d_plot", dims_plot=[], slices=[]):
+        if plot_type not in ["2d_plot", "3d_plot"]:
+            raise Exception("Illegal plot type !")
 
-    if plot_type == "2d_plot" :
-        if len(dims_plot) != 2:
-            raise Exception("Make sure that dim_plot size is 2 !!")
+        if plot_type == "2d_plot":
+            if len(dims_plot) != 2:
+                raise Exception("Make sure that dim_plot size is 2 !!")
 
-    if plot_type == "3d_plot" :
-        if len(dims_plot) != 3:
-            raise Exception("Make sure that dim_plot size is 3 !!")
+        if plot_type == "3d_plot":
+            if len(dims_plot) != 3:
+                raise Exception("Make sure that dim_plot size is 3 !!")
 
-    self.dims_plot = dims_plot
-    self.plot_type = plot_type
-    self.slices = slices
-
-
+        self.dims_plot = dims_plot
+        self.plot_type = plot_type
+        self.slices = slices
