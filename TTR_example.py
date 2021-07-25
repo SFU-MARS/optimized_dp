86f339eb86bbf211e48ad7fb5f3aa98c413e3f63
Minh Bui <buiminhb@sfu.ca>
2021-07-25 10:45:20 -0700

    Updated TTR example and added time to reach computation for 4D system

--- TTR_example.py
+++ TTR_example.py
@@ -10,11 +10,40 @@ from plot_options import *
 from solver import *
 import math
 
-g = Grid(np.array([-3.0, -1.0, -math.pi]), np.array([3.0, 4.0, math.pi]), 3, np.array([160, 160, 160]), [2])
-my_car = DubinsCar()
+# Compute BRS only
+g = Grid(minBounds=np.array([-3.0, -1.0, -math.pi]), maxBounds=np.array([3.0, 4.0, math.pi]),
+         dims=3, pts_each_dim=np.array([80, 80, 80]), periodicDims=[2])
+# Car is trying to reach the target
+my_car = DubinsCar(uMode="min")
 
-Initial_value_f = CylinderShape(g, [2], np.array([0.0, 1.0, 0.0, 0.0]), 0.70)
-po = PlotOptions("3d_plot", [0,1,2], [])
-eps = 0.00001
-V_0 = TTRSolver(my_car, g, Initial_value_f, eps, po)
-np.save("tt2_array.npy", V_0)
+# Initialize target set as a cylinder
+targeSet = CylinderShape(g, [2], np.array([0.0, 1.0, 0.0]), 0.70)
+po = PlotOptions("3d_plot", plotDims=[0,1,2], slicesCut=[],
+                min_isosurface=0, max_isosurface=0)
+
+lookback_length = 1.5
+t_step = 0.05
+small_number = 1e-5
+tau = np.arange(start=0, stop=lookback_length + small_number, step=t_step)
+
+compMethod = { "PrevSetsMode": "minVWithV0"}
+accuracy = "low"
+correct_result = HJSolver(my_car, g, targeSet,
+                          tau, compMethod, po, accuracy)
+
+# -------------------------------- ONE-SHOT TTR COMPUTATION ---------------------------------- #
+g = Grid(minBounds=np.array([-3.0, -1.0, -math.pi]), maxBounds=np.array([3.0, 4.0, math.pi]),
+         dims=3, pts_each_dim=np.array([50, 50, 50]), periodicDims=[2])
+# Car is trying to reach the target
+my_car = DubinsCar(uMode="min")
+
+# Initialize target set as a cylinder
+targetSet = CylinderShape(g, [2], np.array([0.0, 1.0, 0.0]), 0.70)
+po = PlotOptions( "3d_plot", plotDims=[0,1,2], slicesCut=[],
+                  min_isosurface=lookback_length, max_isosurface=lookback_length)
+
+# First compute TTR set
+epsilon = 0.001
+V_0 = TTRSolver(my_car, g, targetSet, epsilon, po)
+
+#np.save("tt2_array.npy", V_0)
