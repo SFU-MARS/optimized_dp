31bfc7cb9cad43a6424ae3845bf5d5225db1dbcd
Michael Lu <lu.michael@outlook.com>
2021-12-08 11:43:23 -0800

    add Air3D dynamics

---
+++ dynamics/Air3D.py
@@ -0,0 +1,68 @@
+import heterocl as hcl
+import numpy as np
+import time
+
+class Air3D:
+    def __init__(self, x=[0,0,0], omega_max=1.1, velocity=0.75, angle_alpha = 1.0, dMax=[0,0,0.0], uMode="min", dMode="max"):
+        self.x = x
+        # assume that both systems use [-omega_max, omega_max] their control 
+        self.omega_max = omega_max
+        self.velocity = velocity
+        self.dMax = dMax
+        self.uMode = uMode
+        self.dMode = dMode
+
+    def opt_ctrl(self, t, state, spat_deriv):
+        opt_w = hcl.scalar(self.omega_max, "opt_w")
+        # Just create and pass back, even though they're not used
+        in3 = hcl.scalar(0, "in3")
+        in4 = hcl.scalar(0, "in4")
+
+        det = hcl.scalar(0, 'det')
+        det[0] = spat_deriv[0] * state[1] - spat_deriv[1] * state[0] - spat_deriv[2]
+
+        with hcl.if_(self.uMode == "min"):
+            with hcl.if_(det > 0):
+                    opt_w[0] = -opt_w
+
+        with hcl.if_(self.uMode == 'max'):
+            with hcl.if_(det < 0):
+                    opt_w[0] = -opt_w
+        return opt_w[0], in3[0], in4[0]
+
+    def opt_dstb(self, t, state, spat_deriv):
+        """
+        :param spat_deriv: tuple of spatial derivative in all dimensions
+        :return: a tuple of optimal disturbances
+        """
+        # Graph takes in 3 possible inputs, by default, for now
+        opt_d = hcl.scalar(self.omega_max, "opt_d")
+        d2 = hcl.scalar(0, "d2")
+        # Just create and pass back, even though they're not used
+        d3 = hcl.scalar(0, "d3")
+
+        with hcl.if_(self.uMode == "min"):
+            with hcl.if_(spat_deriv[0] >= 0):
+                    opt_d[0] = -opt_d
+
+        with hcl.if_(self.uMode == "max"):
+            with hcl.if_(spat_deriv[2] < 0):
+                    opt_d[0] = -opt_d
+
+        return opt_d[0], d2[0], d3[0]
+
+    def dynamics(self, t, state, uOpt, dOpt):
+        """
+        \dot x = -v_a + v_b \cos \psi + a y
+        \dot y = v_b \sin \psi + a y
+        \dot \psi = b- a
+        """
+        x_dot = hcl.scalar(0, "x_dot")
+        y_dot = hcl.scalar(0, "y_dot")
+        theta_dot = hcl.scalar(0, "theta_dot")
+
+        x_dot[0] = -self.velocity + self.velocity * hcl.cos(state[2]) + uOpt[0] * state[1]
+        y_dot[0] = self.velocity * hcl.sin(state[2]) - uOpt[0] * state[0]
+        theta_dot[0] = dOpt[0] - uOpt[0]
+
+        return x_dot[0], y_dot[0], theta_dot[0]
---
+++ user_definer_deepreach.py
@@ -0,0 +1,56 @@
+import numpy as np
+# Utility functions to initialize the problem
+from Grid.GridProcessing import Grid
+from Shapes.ShapesFunctions import *
+# Specify the  file that includes dynamic systems
+from dynamics.Air3D import Air3D
+# Plot options
+from plot_options import *
+# Solver core
+from solver import HJSolver
+
+import math
+""" USER INTERFACES
+- Define grid
+- Generate initial values for grid using shape functions
+- Time length for computations
+- Initialize plotting option
+- Call HJSolver function
+"""
+
+# Scenario 1
+g = Grid(np.array([-1.0, -1.0, -math.pi]), np.array([1.0, 1.0, math.pi]), 3, np.array([101, 101, 101]), [2])
+r = 0.25
+Initial_value_f = CylinderShape(g, [2], np.zeros(3), r) # rad of obstacle + robot radius
+
+# Look-back lenght and time step
+lookback_length = 2.0
+t_step = 0.05
+
+small_number = 1e-5
+tau = np.arange(start=0, stop=lookback_length + small_number, step=t_step)
+my_car = Air3D(uMode='max', dMode='min')
+
+po = PlotOptions(do_plot=True, plot_type="3d_plot", plotDims=[0,1,2], slicesCut=[])
+
+"""
+Assign one of the following strings to `PrevSetMode` to specify the characteristics of computation
+"PrevSetMode":
+{
+"none" -> compute Backward Reachable Set, 
+"minVWithV0" -> compute Backward Reachable Tube,
+"maxVWithVInit" -> compute max V over time,
+"minVWithVInit" compute min V over time,
+}
+
+(optional)
+"TargetSetMode":
+{
+"min" -> min with target set,
+"max" -> max with taget set
+}
+"""
+
+compMethods = { "PrevSetsMode": "minVWithV0"}
+V = HJSolver(my_car, g, Initial_value_f, tau, compMethods, po)
+np.save(f'V_air3D_{r}', V)
