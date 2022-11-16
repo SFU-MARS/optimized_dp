d6a389b6120a22149538ef890cefb20a8a5d4060
hha160 <HuHY97@outlook.com>
2022-11-15 16:10:27 -0800

    "revise: add the whole logic in this file."

--- MRAG/AD_Game.py
+++ MRAG/AD_Game.py
@@ -2,12 +2,10 @@ import numpy as np
 from odp.Plots.plotting_utilities import plot_2d
 from odp.Grid import Grid
 from odp.solver import HJSolver, computeSpatDerivArray
+from MaximumMatching import MaxMatching
 
+# initialize attackers and defenders
 
-# plot_2d figure
-# g must be the same as one in the ValueFunction.py
-g = Grid(np.array([-1.0, -1.0, -1.0, -1.0]), np.array([1.0, 1.0, 1.0, 1.0]), 4, np.array([31, 31, 31, 31]))
-value_function = np.load('1v1AttackDefend.npy')
-V_2D = value_function[:, :, 29, 29, 0]  # 0 is reachable set, -1 is target set
-plot_2d(g, V_2D=V_2D)
+# initialize the maximum_matching logic
 
+# apply control to the agents
