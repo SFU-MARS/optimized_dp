fcee7209b21ba86ab3e4386de3c7d193bb09d025
hha160 <HuHY97@outlook.com>
2022-11-16 17:24:40 -0800

    "revise: change different positions of the defender, but still something wrong."

--- MRAG/plot_figures.py
+++ MRAG/plot_figures.py
@@ -8,7 +8,7 @@ from odp.solver import HJSolver, computeSpatDerivArray
 # g must be the same as one in the ValueFunction.py
 g = Grid(np.array([-1.0, -1.0, -1.0, -1.0]), np.array([1.0, 1.0, 1.0, 1.0]), 4, np.array([31, 31, 31, 31]))
 value_function = np.load('1v1AttackDefend.npy')
-V_2D = value_function[:, :, 29, 29, -1]  # 0 is reachable set, -1 is target set
+V_2D = value_function[:, :, 20, 23, 0]  # 0 is reachable set, -1 is target set
 plot_2d(g, V_2D=V_2D)
 
 # plot the 2D map
