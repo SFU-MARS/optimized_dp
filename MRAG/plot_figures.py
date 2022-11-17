7b53fdb9d946d740113bb15ca26bf60e6ce409fc
hha160 <HuHY97@outlook.com>
2022-11-16 16:39:09 -0800

    "revise: check different time span 0."

--- MRAG/plot_figures.py
+++ MRAG/plot_figures.py
@@ -8,33 +8,33 @@ from odp.solver import HJSolver, computeSpatDerivArray
 # g must be the same as one in the ValueFunction.py
 g = Grid(np.array([-1.0, -1.0, -1.0, -1.0]), np.array([1.0, 1.0, 1.0, 1.0]), 4, np.array([31, 31, 31, 31]))
 value_function = np.load('1v1AttackDefend.npy')
-V_2D = value_function[:, :, 29, 29, 0]  # 0 is reachable set, -1 is target set
+V_2D = value_function[:, :, 29, 29, -1]  # 0 is reachable set, -1 is target set
 plot_2d(g, V_2D=V_2D)
 
 # plot the 2D map
-plt.xlim((-1.0, 1.0))
-plt.ylim((-1.0, 1.0))
-plt.xlabel('x-label')
-plt.ylabel('y-label')
-# obs1
-plt.hlines(y=-0.3, xmin=-0.1, xmax=0.1, colors='blue', linewidth=2.0)
-plt.hlines(y=-1.0, xmin=-0.1, xmax=0.1, colors='blue', linewidth=2.0)
-plt.vlines(x=-0.1, ymin=-1.0, ymax=-0.3, colors='blue', linewidth=2.0)
-plt.vlines(x=0.1, ymin=-1.0, ymax=-0.3, colors='blue', linewidth=2.0)
-# obs2
-plt.hlines(y=0.6, xmin=-0.1, xmax=0.1, colors='blue', linewidth=2.0)
-plt.hlines(y=0.3, xmin=-0.1, xmax=0.1, colors='blue', linewidth=2.0)
-plt.vlines(x=-0.1, ymin=0.3, ymax=0.6, colors='blue', linewidth=2.0)
-plt.vlines(x=0.1, ymin=0.3, ymax=0.6, colors='blue', linewidth=2.0)
-# target
-plt.hlines(y=0.1, xmin=0.6, xmax=0.8, colors='green', linewidth=2.0)
-plt.hlines(y=0.3, xmin=0.6, xmax=0.8, colors='green', linewidth=2.0)
-plt.vlines(x=0.6, ymin=0.1, ymax=0.3, colors='green', linewidth=2.0)
-plt.vlines(x=0.8, ymin=0.1, ymax=0.3, colors='green', linewidth=2.0)
-# legends
-plt.text(-0.05, -0.60, 'obs1')
-plt.text(-0.05, 0.42, 'obs2')
-plt.text(0.62, 0.18, 'target')
-plt.show()
+# plt.xlim((-1.0, 1.0))
+# plt.ylim((-1.0, 1.0))
+# plt.xlabel('x-label')
+# plt.ylabel('y-label')
+# # obs1
+# plt.hlines(y=-0.3, xmin=-0.1, xmax=0.1, colors='blue', linewidth=2.0)
+# plt.hlines(y=-1.0, xmin=-0.1, xmax=0.1, colors='blue', linewidth=2.0)
+# plt.vlines(x=-0.1, ymin=-1.0, ymax=-0.3, colors='blue', linewidth=2.0)
+# plt.vlines(x=0.1, ymin=-1.0, ymax=-0.3, colors='blue', linewidth=2.0)
+# # obs2
+# plt.hlines(y=0.6, xmin=-0.1, xmax=0.1, colors='blue', linewidth=2.0)
+# plt.hlines(y=0.3, xmin=-0.1, xmax=0.1, colors='blue', linewidth=2.0)
+# plt.vlines(x=-0.1, ymin=0.3, ymax=0.6, colors='blue', linewidth=2.0)
+# plt.vlines(x=0.1, ymin=0.3, ymax=0.6, colors='blue', linewidth=2.0)
+# # target
+# plt.hlines(y=0.1, xmin=0.6, xmax=0.8, colors='green', linewidth=2.0)
+# plt.hlines(y=0.3, xmin=0.6, xmax=0.8, colors='green', linewidth=2.0)
+# plt.vlines(x=0.6, ymin=0.1, ymax=0.3, colors='green', linewidth=2.0)
+# plt.vlines(x=0.8, ymin=0.1, ymax=0.3, colors='green', linewidth=2.0)
+# # legends
+# plt.text(-0.05, -0.60, 'obs1')
+# plt.text(-0.05, 0.42, 'obs2')
+# plt.text(0.62, 0.18, 'target')
+# plt.show()
 
 
