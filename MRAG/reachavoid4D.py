06f7d3119299d7fce78269ea0bca18270ce0ec6b
hha160 <HuHY97@outlook.com>
2022-11-22 15:57:29 -0800

    "feat: AttackDefender4D0.py is the former dynamics I designed; reachavoid4D.py is from Kyra."

---
+++ MRAG/reachavoid4D.py
@@ -0,0 +1,186 @@
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+import heterocl as hcl
+import math
+import numpy as np
+import plotly.graph_objects as go
+from odp.Grid import Grid
+# Utility functions to initialize the problem
+from odp.Grid import Grid
+from odp.Shapes import *
+
+# Specify the  file that includes dynamic systems
+from odp.dynamics.AttackerDefender4D import AttackerDefender4D
+# Plot options
+from odp.Plots import PlotOptions
+from odp.Plots.plotting_utilities import plot_2d
+# Solver core
+from odp.solver import HJSolver, computeSpatDerivArray
+import math
+
+'''
+# Specify the  file that includes dynamic systems
+from odp.dynamics import DubinsCapture
+from odp.dynamics import DubinsCar4D2
+'''
+from odp.Plots import PlotOptions
+from odp.solver import HJSolver
+from odp.Plots.plotting_utilities import *
+#from odp.Plots.plot_contour_5D import *
+
+class ReachAvoid:
+    '''
+    Agent1 : Attacker
+    Agent2 : Defender
+    '''
+    def __init__(self, x = [0,0,0,0], uMax = 1, uMin = -1, dMax = 1, dMin = -1, uMode = 'min', dMode = 'max', speed_a = 1, speed_b = 1):
+        self.x = x
+        self.uMax = uMax
+        self.uMin = uMin
+
+        self.dMax = dMax
+        self.dMin = dMin
+        # QUESTION: uMode and dMode for reach-avoid?
+        self.uMode = uMode
+        self.dMode = dMode
+        # max speed for attacker and defender 
+        self.speed_a = speed_a
+        self.speed_b = speed_b
+
+    def dynamics(self, t, state, uOpt, dOpt):
+        x1_dot = hcl.scalar(0, "x_1_dot")
+        x2_dot = hcl.scalar(0, "x_2_dot")
+        x3_dot = hcl.scalar(0, "x_3_dot")
+        x4_dot = hcl.scalar(0, "x_4_dot")
+       
+        x1_dot[0] = self.speed_a * uOpt[0]
+        x2_dot[0] = self.speed_a * uOpt[1]
+        x3_dot[0] = self.speed_b * dOpt[0]
+        x4_dot[0] = self.speed_b * dOpt[1]
+       
+        return (x1_dot[0], x2_dot[0], x3_dot[0], x4_dot[0])
+
+    def opt_ctrl(self, t, state, spat_deriv):
+        # optimal control is (x1_dot, x2_dot), the direction to multiply by the scalar
+        uOpt_x = hcl.scalar(0, "u")
+        uOpt_y = hcl.scalar(0, "a")
+        # Just create and pass back, even though they're not used
+        in3   = hcl.scalar(0, "in3")
+        in4   = hcl.scalar(0, "in4")
+        # declare the hcl scalars for relevant spat_derivs
+        deriv1 = hcl.scalar(0, "deriv1")
+        deriv2 = hcl.scalar(0, "deriv2")
+        deriv1[0] = spat_deriv[0]
+        deriv2[0] = spat_deriv[1]
+        ctrl_len = hcl.sqrt(deriv1[0] * deriv1[0] + deriv2[0] * deriv2[0])
+        if self.uMode == "min":
+            uOpt_x[0] = -1.0 * deriv1[0] / ctrl_len
+            uOpt_y[0] = -1.0 * deriv2[0] / ctrl_len
+        else:
+            uOpt_x[0] = deriv1[0]/ ctrl_len
+            uOpt_y[0] = deriv2[0] / ctrl_len
+
+        return (uOpt_x[0], uOpt_y[0], in3[0], in4[0])
+
+    def opt_dstb(self, t, state, spat_deriv):
+        dOpt_x = hcl.scalar(0, "d_x")
+        dOpt_y = hcl.scalar(0, "d_y")
+        # Just create and pass back, even though they're not used
+        d3 = hcl.scalar(0, "d3")
+        d4 = hcl.scalar(0, "d4")
+
+        deriv1 = hcl.scalar(0, "deriv1")
+        deriv2 = hcl.scalar(0, "deriv2")
+        deriv1[0] = spat_deriv[2] 
+        deriv2[0] = spat_deriv[3] 
+        dstb_len = hcl.sqrt(deriv1[0] * deriv1[0] + deriv2[0] * deriv2[0])
+      
+        if self.dMode == "max":
+            
+            dOpt_x[0] = deriv1[0] / dstb_len
+            dOpt_y[0] = deriv2[0] / dstb_len
+        else:
+           
+            dOpt_x[0] = -1 * deriv1[0]/ dstb_len
+            dOpt_y[0] = -1 * deriv2[0] / dstb_len
+           
+        return (dOpt_x[0], dOpt_y[0], d3[0], d4[0]) 
+    
+    def capture_set(self, grid, capture_radius, mode):
+        
+        data = np.zeros(grid.pts_each_dim)
+
+        data = data + np.power(grid.vs[0] - grid.vs[2], 2)
+        data = data + np.power(grid.vs[1] - grid.vs[3], 2)
+       # data = np.sqrt(data) - radius
+        if mode == "capture":
+            return np.sqrt(data) - capture_radius
+        if mode == "escape":
+            return capture_radius - np.sqrt(data)
+           
+        
+
+def main():
+    g = Grid(np.array([-1.0, -1.0, -1.0, -1.0]), np.array([1.0, 1.0, 1.0, 1.0]),
+            4, np.array([31, 31, 31, 31]))
+    # Initialize the dynamics. We assume the attackers' perspective.
+    attacker_perpective = ReachAvoid(uMode = 'min', dMode = 'max')
+
+    # static obstacles from the baseline paper
+    obs1 = ShapeRectangle(g, [-0.1, -1.0, -1000, -1000], [0.1, -0.3, 1000, 1000])  # attacker stuck in obs1
+    obs2 = ShapeRectangle(g, [-0.1, 0.30, -1000, -1000], [0.1, 0.60, 1000, 1000])  # attacker stuck in obs2
+    obstacle = np.minimum(obs1, obs2)
+
+    obs1_reach = ShapeRectangle(g, [-1000, -1000, -0.1, -1.0], [1000, 1000, 0.1, -0.3])  # defender stuck in obs1
+    obs2_reach = ShapeRectangle(g, [-1000, -1000, -0.1, 0.30], [1000, 1000, 0.1, 0.60])  # defender stuck in obs2
+    obstacle_reach = np.minimum(obs1_reach, obs2_reach)
+    
+    goal_a = ShapeRectangle(g, [0.6, 0.1, -1000, -1000], [0.8, 0.3, 1000, 1000])  # attacker arrives target
+
+    capture_region = attacker_perpective.capture_set(g, 0.3, "capture")
+    escape_region = attacker_perpective.capture_set(g, 0.3, "escape")
+    #reach = np.maximum(goal_a, escape_region)
+    reach = np.minimum(np.maximum(goal_a, escape_region), obstacle_reach)
+    avoid = np.minimum(obstacle, capture_region)
+
+    po1 = PlotOptions(do_plot=False, plot_type="3d_plot", plotDims=[0,1,2],
+                  slicesCut=[3])
+   # plot_isosurface(g, avoid, po1) #plot the reach and avoid sets for debugging.
+   # plot_isosurface(g, reach, po1)
+
+    lookback_length = 5.0
+    t_step = 0.05
+    small_number = 1e-5
+    tau = np.arange(start=0, stop=lookback_length + small_number, step=t_step)
+
+    # Get our first safety bubble.
+    po2 = PlotOptions(do_plot=False, plot_type = "3d_plot", plotDims = [0,1,3], slicesCut=[2])
+    compMethods = { "TargetSetMode": "minVWithVTarget",
+                "ObstacleSetMode": "maxVWithObstacle"}
+                
+    # HJSolver(dynamics object, g, initial value function, time length, system objectives, plotting options)
+    result = HJSolver(attacker_perpective, g, [reach, avoid], tau, compMethods, po2, saveAllTimeSteps=True)
+    np.save('result.npy',result)
+    print(result.shape)
+    # print(result[0], result[30][30][30][30], result[15][15][15][15], result[15][10][15][10], result[0][15][1][15])
+
+    
+
+    x1_idx = [0, 10, 20, 30]
+    x2_idx = [0, 10, 20, 30]
+    #x4_idx = [0, 10, 20, 30]
+# loop over these indices in the g for fixed values
+#     for idx1 in x1_idx:
+#         for idx2 in x2_idx:
+#
+#             plot_reachavoid(result, [idx1, idx2], 2)
+
+    return
+
+if __name__ == "__main__":
+    
+    main()
+  
---
+++ odp/dynamics/AttackerDefender4D0.py
@@ -0,0 +1,176 @@
+import heterocl as hcl
+import math
+import numpy as np
+
+""" 4D 1v1 AttackerDefender DYNAMICS IMPLEMENTATION 
+ xA1_dot = vA * u1
+ xA2_dot = vA * u2
+ xD1_dot = vD * d1
+ xD2_dot = vD * d2
+ """
+
+
+class AttackerDefender4D:
+    def __init__(self, x=[0, 0, 0, 0], uMin=[-1, -1], uMax=[1, 1], dMin=[-1, -1],
+                 dMax=[1, 1], uMode="min", dMode="max"):
+        """Creates a Dublin Car with the following states:
+           X position, Y position, speed, heading
+           The controls are the acceleration and turn rate (angular speed)
+           The disturbances are the noise in the velocity components.
+        Args:
+            x (list, optional): Initial state . Defaults to [0,0,0,0].
+            uMin (list, optional): Lowerbound of user control. Defaults to [-1,-1].
+            uMax (list, optional): Upperbound of user control.
+                                   Defaults to [1,1].
+            dMin (list, optional): Lowerbound of disturbance to user control, . Defaults to [-0.25,-0.25].
+            dMax (list, optional): Upperbound of disturbance to user control. Defaults to [0.25,0.25].
+            uMode (str, optional): Accepts either "min" or "max".
+                                   * "min" : have optimal control reach goal
+                                   * "max" : have optimal control avoid goal
+                                   Defaults to "min".
+            dMode (str, optional): Accepts whether "min" or "max" and should be opposite of uMode.
+                                   Defaults to "max".
+        """
+        self.x = x
+        self.uMax = uMax
+        self.uMin = uMin
+        self.dMax = dMax
+        self.dMin = dMin
+        assert (uMode in ["min", "max"])
+        self.uMode = uMode
+        if uMode == "min":
+            assert (dMode == "max")
+        else:
+            assert (dMode == "min")
+        self.dMode = dMode
+
+    def opt_ctrl(self, t, state, spat_deriv):
+        """
+        :param t: time t
+        :param state: tuple of coordinates
+        :param spat_deriv: tuple of spatial derivative in all dimensions
+        :return:
+        """
+        # System dynamics
+        # xA1_dot = vA * u1
+        # xA2_dot = vA * u2
+        # xD1_dot = vD * d1
+        # xD2_dot = vD * d2
+
+        # Graph takes in 4 possible inputs, by default, for now
+        # In 1v1AttackerDefender, a(t) = [a1, a2]^T
+        opt_a1 = hcl.scalar(self.uMax[0], "opt_a1")
+        opt_a2 = hcl.scalar(self.uMax[1], "opt_a2")
+        # Just create and pass back, even though they're not used
+        in3 = hcl.scalar(0, "in3")
+        in4 = hcl.scalar(0, "in4")
+
+        if self.uMode == "min":
+            with hcl.if_(spat_deriv[0] > 0):
+                opt_a1[0] = self.uMin[0]  # now is Bang-bang control and I should revise it into equation (11)
+            with hcl.if_(spat_deriv[1] > 0):
+                opt_a2[0] = self.uMin[1]
+        else:
+            with hcl.if_(spat_deriv[0] < 0):
+                opt_a1[0] = self.uMin[0]
+            with hcl.if_(spat_deriv[1] < 0):
+                opt_a2[0] = self.uMin[1]
+        # return 3, 4 even if you don't use them
+        return opt_a1[0], opt_a2[0], in3[0], in4[0]
+
+    def opt_dstb(self, t, state, spat_deriv):
+        """
+        :param spat_deriv: tuple of spatial derivative in all dimensions
+        :return: a tuple of optimal disturbances
+        """
+        # Graph takes in 4 possible inputs, by default, for now
+        d1 = hcl.scalar(0, "d1")
+        d2 = hcl.scalar(0, "d2")
+        # Just create and pass back, even though they're not used
+        d3 = hcl.scalar(0, "d3")
+        d4 = hcl.scalar(0, "d4")
+
+        # with hcl.if_(self.dMode == "max"):
+        # I should clarify the expression the d(t) using equation (12)
+        if self.dMode == "max":
+            with hcl.if_(spat_deriv[0] > 0):
+                d1[0] = self.dMax[0]
+            with hcl.elif_(spat_deriv[0] < 0):
+                d1[0] = self.dMin[0]
+            with hcl.if_(spat_deriv[1] > 0):
+                d2[0] = self.dMax[1]
+            with hcl.elif_(spat_deriv[1] < 0):
+                d2[0] = self.dMin[1]
+        else:
+            with hcl.if_(spat_deriv[0] > 0):
+                d1[0] = self.dMin[0]
+            with hcl.elif_(spat_deriv[0] < 0):
+                d1[0] = self.dMax[0]
+            with hcl.if_(spat_deriv[1] > 0):
+                d2[0] = self.dMin[1]
+            with hcl.elif_(spat_deriv[1] < 0):
+                d2[0] = self.dMax[1]
+
+        return (d1[0], d2[0], d3[0], d4[0])
+
+    def dynamics(self, t, state, uOpt, dOpt):
+        # maximum velocity
+        vA = hcl.scalar(1.0, "vA")
+        vD = hcl.scalar(1.0, "vD")
+
+        xA1_dot = hcl.scalar(0, "xA1_dot")
+        xA2_dot = hcl.scalar(0, "xA2_dot")
+        xD1_dot = hcl.scalar(0, "xD1_dot")
+        xD2_dot = hcl.scalar(0, "xD2_dot")
+
+        xA1_dot[0] = vA * uOpt[0]
+        xA2_dot[0] = vA * uOpt[1]
+        xD1_dot[0] = vD * dOpt[0]
+        xD2_dot[0] = vD * dOpt[1]
+
+        return xA1_dot[0], xA2_dot[0], xD1_dot[0], xD2_dot[0]
+
+        # The below function can have whatever form or parameters users want
+        # These functions are not used in HeteroCL program, hence is pure Python code and
+        # can be used after the value function has been obtained.
+
+    def optCtrl_inPython(self, spat_deriv):
+        """
+        :param t: time t
+        :param state: tuple of coordinates
+        :param spat_deriv: tuple of spatial derivative in all dimensions
+        :return:
+        """
+        # System dynamics
+        # xA1_dot = vA * u1
+        # xA2_dot = vA * u2
+        # xD1_dot = vD * d1
+        # xD2_dot = vD * d2
+        opt_a = self.uMax[0]
+        opt_w = self.uMax[1]
+
+        # The initialized control only change sign in the following cases
+        if self.uMode == "min":
+            if spat_deriv[2] > 0:
+                opt_a = self.uMin[0]
+            if spat_deriv[3] > 0:
+                opt_w = self.uMin[1]
+        else:
+            if spat_deriv[2] < 0:
+                opt_a = self.uMin[0]
+            if spat_deriv[3] < 0:
+                opt_w = self.uMin[1]
+
+        return opt_a, opt_w
+
+    def capture_set(self, grid, capture_radius, mode):
+        # this function is the distance between 1 attacker and 1 defender
+        data = np.zeros(grid.pts_each_dim)
+
+        data = data + np.power(grid.vs[0] - grid.vs[2], 2)
+        data = data + np.power(grid.vs[1] - grid.vs[3], 2)
+        # data = np.sqrt(data) - radius
+        if mode == "capture":
+            return np.sqrt(data) - capture_radius
+        if mode == "escape":
+            return capture_radius - np.sqrt(data)
