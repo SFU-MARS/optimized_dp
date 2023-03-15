540dcf4f329ae95c46d0bffb9a5a2bd85609cabb
hha160 <HuHY97@outlook.com>
2023-03-15 16:58:41 -0700

    feat: adjust the plotting codes based on the index logic.

--- MRAG/plot_figures.py
+++ MRAG/plot_figures.py
@@ -1,65 +1,50 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from odp.Plots.plotting_utilities import plot_2d, plot_2d_with_avoid
+from odp.Plots.plotting_utilities import plot_2d, plot_game
 from odp.Grid import Grid
-from utilities import lo2slice1v1, check1v1
+from utilities import lo2slice1v1, lo2slice2v1
 from odp.solver import HJSolver, computeSpatDerivArray
 import math
 
-# plot value_function in 2d figure
-# g must be the same as one in the ValueFunction.py
-grids = Grid(np.array([-1.0, -1.0, -1.0, -1.0]), np.array([1.0, 1.0, 1.0, 1.0]), 4, np.array([45, 45, 45, 45]))
-value_function = np.load('MRAG/1v1AttackDefend.npy')
-print(f'The shape of the value function is {value_function.shape} \n')
-# define the locations of the defender
-x_d = -0.3
-y_d = 0.5
-x_defender, y_defender = lo2slice1v1(x_location=x_d, y_location=y_d, slices=45)
-x1, y1 = lo2slice1v1(x_location=0, y_location=0, slices=45)
-print(f'The defender is at the location [{x_d}, {y_d}] \n')
-V_2D = value_function[:, :, x_defender, y_defender]  # , 0] if the saveAllTimeSteps=True. 0 is reachable set, -1 is target set
-print(f'The value function of the attacker at the location (0, 0) is {value_function[x1, y1, x_defender, y_defender]}. \n')
-print(f'The shape of the V_2D is {V_2D.shape}. \n')
-plot_2d(grids, V_2D=V_2D)
+# plot 1v1 reach-avoid game
+grids1v1 = Grid(np.array([-1.0, -1.0, -1.0, -1.0]), np.array([1.0, 1.0, 1.0, 1.0]), 4, np.array([45, 45, 45, 45]))
+value1v1 = np.load('MRAG/1v1AttackDefend.npy')
+print(f'The shape of the value function is {value1v1.shape} \n')
+# define the joint states of (a1x, a1y, d1x, d1y)
+a1x = 0
+a1y = 0
+d1x = -0.3
+d1y = 0.5
+jointstates1v1 = (a1x, a1y, d1x, d1y)
+a1x_slice, a1y_slice, d1x_slice, d1y_slice = lo2slice1v1(jointstates1v1, slices=45)
+print(f'The attacker is at the location [{a1x}, {a1y}] and the defender is at the location [{d1x}, {d1y}] \n')
+print(f'The value function of the attacker at the location (0, 0) is {value1v1[a1x_slice, a1y_slice, d1x_slice, d1y_slice]}. \n')
+value_function1v1 = value1v1[:, :, d1x_slice, d1y_slice]  # , 0] if the saveAllTimeSteps=True. 0 is reachable set, -1 is target set
+print(f'The shape of the 1v1 value function is {value_function1v1.shape}. \n')
+# plot_2d(grids1v1, value_function1v1)
+# if want to add the positons of attackers and defenders
+attackers = [(a1x, a1y)]
+defenders = [(d1x, d1y)]
+plot_game(grids1v1, value_function1v1, attackers, defenders)
 
-# plot value_function in 2d figure for debug
-# g must be the same as one in the ValueFunction.py
-# g_debug = Grid(np.array([-1.0, -1.0, -math.pi]), np.array([1.0, 1.0, math.pi]), 3, np.array([40, 40, 40]), [2])
-# value_function = np.load('example_reach_avoid.npy')
-# print(f'The shape of the value function is {value_function.shape} \n')
-# # define the locations of the defender
-# # x_d = -0.3
-# # y_d = 0.5
-# # x_defender, y_defender = loca2slices(x_location=x_d, y_location=y_d, slices=45)
-# # print(f'The defender is at the location [{x_d}, {y_d}] \n')
-# V_2D = value_function[:, :, 20, -1]  # 0 is reachable set, -1 is target set
-# plot_2d(g_debug, V_2D=V_2D)
-
-
-# plot the 2D map
-# plt.xlim((-1.0, 1.0))
-# plt.ylim((-1.0, 1.0))
-# plt.xlabel('x-label')
-# plt.ylabel('y-label')
-# # obs1
-# plt.hlines(y=-0.3, xmin=-0.1, xmax=0.1, colors='blue', linewidth=2.0)
-# plt.hlines(y=-1.0, xmin=-0.1, xmax=0.1, colors='blue', linewidth=2.0)
-# plt.vlines(x=-0.1, ymin=-1.0, ymax=-0.3, colors='blue', linewidth=2.0)
-# plt.vlines(x=0.1, ymin=-1.0, ymax=-0.3, colors='blue', linewidth=2.0)
-# # obs2
-# plt.hlines(y=0.6, xmin=-0.1, xmax=0.1, colors='blue', linewidth=2.0)
-# plt.hlines(y=0.3, xmin=-0.1, xmax=0.1, colors='blue', linewidth=2.0)
-# plt.vlines(x=-0.1, ymin=0.3, ymax=0.6, colors='blue', linewidth=2.0)
-# plt.vlines(x=0.1, ymin=0.3, ymax=0.6, colors='blue', linewidth=2.0)
-# # target
-# plt.hlines(y=0.1, xmin=0.6, xmax=0.8, colors='green', linewidth=2.0)
-# plt.hlines(y=0.3, xmin=0.6, xmax=0.8, colors='green', linewidth=2.0)
-# plt.vlines(x=0.6, ymin=0.1, ymax=0.3, colors='green', linewidth=2.0)
-# plt.vlines(x=0.8, ymin=0.1, ymax=0.3, colors='green', linewidth=2.0)
-# # legends
-# plt.text(-0.05, -0.60, 'obs1')
-# plt.text(-0.05, 0.42, 'obs2')
-# plt.text(0.62, 0.18, 'target')
-# plt.show()
-
-
+# plot for 2v1 game
+grid2v1 = Grid(np.array([-1.0, -1.0, -1.0, -1.0, -1.0, -1.0]), np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0]), 6, np.array([30, 30, 30, 30, 30, 30])) # original 45, on mars-14 20 is the upper bound
+value2v1 = np.load('MRAG/2v1AttackDefend.npy')
+print(f'The shape of the value function is {value2v1.shape} \n')
+# define the joint states of (a1x, a1y, a2x, a2y, d1x, d1y)
+a1x = 0
+a1y = 0
+a2x = -0.3
+a2y = -0.5
+d1x = -0.3
+d1y = 0.5
+jointstates2v1 = (a1x, a1y, a2x, a2y, d1x, d1y)
+attackers = [(a1x, a1y), (a2x, a2y)]
+defenders = [(d1x, d1y)]
+a1x_slice, a1y_slice, a2x_slice, a2y_slice, d1x_slice, d1y_slice = lo2slice2v1(jointstates2v1, slices=30)
+# 
+value_function2v1 = value2v1[:, :, a2x_slice, a2y_slice, d1x_slice, d1y_slice] 
+print("Min value of the array {}".format(np.min(value_function2v1)))
+print(f'The shape of the 2v1 value function is {value_function2v1.shape}. \n')
+plot_2d(grid2v1, value_function2v1)
+plot_game(grid2v1, value_function2v1, attackers, defenders)
