2ac1cae0163ad9605452e621261de15b28289b0b
buinm <b.minh96@gmail.com>
2023-03-22 16:46:47 -0700

    Add new to compute 6D 2v1 case that guarantees defender to capture both attackers

---
+++ MRAG/hjvalue2v1_new.py
@@ -0,0 +1,94 @@
+import numpy as np
+# Utility functions to initialize the problem
+from odp.Grid import Grid
+from odp.Shapes import *
+# Specify the  file that includes dynamic systems, AttackerDefender4D
+from MRAG.AttackerDefender1v1 import AttackerDefender1v1 
+# Plot options
+from odp.Plots import PlotOptions
+from odp.Plots.plotting_utilities import plot_2d, plot_isosurface
+# Solver core
+from odp.solver import HJSolver, computeSpatDerivArray
+import math
+
+""" USER INTERFACES
+- Define grid
+- Generate initial values for grid using shape functions
+- Time length for computations
+- Initialize plotting option
+- Call HJSolver function
+"""
+
+##################################################### EXAMPLE 4 1v1AttackerDefender ####################################
+#
+grids = Grid(np.array([-1.0, -1.0, -1.0, -1.0]), np.array([1.0, 1.0, 1.0, 1.0]), 4, np.array([30, 30, 30, 30])) # original 45
+
+# Define my object dynamics
+agents_1v1 = AttackerDefender1v1(uMode="min", dMode="max")  # 1v1 (4 dims dynamics)
+
+# Avoid set, no constraint means inf
+obs1_attack = ShapeRectangle(grids, [-0.1, -1.0, -1000, -1000], [0.1, -0.3, 1000, 1000])  # attacker stuck in obs1
+obs2_attack = ShapeRectangle(grids, [-0.1, 0.30, -1000, -1000], [0.1, 0.60, 1000, 1000])  # attacker stuck in obs2
+obs3_capture = agents_1v1.capture_set(grids, 0.1, "capture")  # attacker being captured by defender, try different radius
+avoid_set = np.minimum(obs3_capture, np.minimum(obs1_attack, obs2_attack)) # original
+# debugging
+# avoid_set = np.minimum(obs3_capture, obs2_attack) # debug1
+# obs_circle = CylinderShape(g, [2, 3], np.array([0.0, 0.5, 0.0, 0.0]), 0.3) # debug2 + debug4
+# avoid_set = np.minimum(obs3_capture, obs_circle) # debug2 + debug5
+# avoid_set = obs2_attack  # debug3
+# avoid_set = obs_circle # debug6
+
+# Reach set, run and see what it is!
+goal1_destination = ShapeRectangle(grids, [0.6, 0.1, -1000, -1000], [0.8, 0.3, 1000, 1000])  # attacker arrives target
+goal2_escape = agents_1v1.capture_set(grids, 0.1, "escape")  # attacker escape from defender
+obs1_defend = ShapeRectangle(grids, [-1000, -1000, -0.1, -1000], [1000, 1000, 0.1, -0.3])  # defender stuck in obs1
+obs2_defend = ShapeRectangle(grids, [-1000, -1000, -0.1, 0.30], [1000, 1000, 0.1, 0.60])  # defender stuck in obs2
+reach_set = np.minimum(np.maximum(goal1_destination, goal2_escape), np.minimum(obs1_defend, obs2_defend)) # original
+
+
+# Look-back length and time step
+lookback_length = 4.5  # the same as 2014Mo
+t_step = 0.025
+
+# Actual calculation process, needs to add new plot function to draw a 2D figure
+small_number = 1e-5
+tau = np.arange(start=0, stop=lookback_length + small_number, step=t_step)
+
+# while plotting make sure the len(slicesCut) + len(plotDims) = grid.dims
+po = PlotOptions(do_plot=False, plot_type="2d_plot", plotDims=[0, 1], slicesCut=[22, 22])
+# plot the 2 obs
+# plot_isosurface(g, np.minimum(obs1_attack, obs2_attack), po)
+# plot_isosurface(g, obs3_capture, po)
+
+# In this example, we compute a Reach-Avoid Tube
+compMethods = {"TargetSetMode": "minVWithVTarget", "ObstacleSetMode": "maxVWithObstacle"} # original one
+# compMethods = {"TargetSetMode": "minVWithVTarget"}
+result = HJSolver(agents_1v1, grids, [reach_set, avoid_set], tau, compMethods, po, saveAllTimeSteps=True) # original one
+# result = HJSolver(my_2agents, g, avoid_set, tau, compMethods, po, saveAllTimeSteps=True)
+
+# We just expand this value function to 6D case
+
+attacker1_wins_2v1 = np.zeros((30, 30, 30, 30, 30 ,30)) + np.expand_dims(result[..., 0], axis=(2,3))
+attacker2_wins_2v1 = np.zeros((30, 30, 30, 30, 30 ,30)) + np.expand_dims(result[..., 0], axis=(0,1))
+at_least_one_win_2v1 = np.minimum(attacker2_wins_2v1, attacker1_wins_2v1)
+
+np.save('2v1AttackDefend_new.npy', at_least_one_win_2v1)
+# save the value function
+# np.save('/localhome/hha160/optimized_dp/MRAG/1v1AttackDefend.npy', result)
+
+
+# # Compute spatial derivatives at every state
+# last_time_step_result = result[..., 0]
+# x_derivative = computeSpatDerivArray(g, last_time_step_result, deriv_dim=1, accuracy="low")
+# y_derivative = computeSpatDerivArray(g, last_time_step_result, deriv_dim=2, accuracy="low")
+# v_derivative = computeSpatDerivArray(g, last_time_step_result, deriv_dim=3, accuracy="low")
+# T_derivative = computeSpatDerivArray(g, last_time_step_result, deriv_dim=4, accuracy="low")
+#
+# # Let's compute optimal control at some random idices
+# spat_deriv_vector = (x_derivative[10,20,15,15], y_derivative[10,20,15,15],
+#                      v_derivative[10,20,15,15], T_derivative[10,20,15,15])
+#
+# # Compute the optimal control
+# opt_a, opt_w = my_2agents.optCtrl_inPython(spat_deriv_vector)
+# print("Optimal accel is {}\n".format(opt_a))
+# print("Optimal rotation is {}\n".format(opt_w))
--- MRAG/plot_figures.py
+++ MRAG/plot_figures.py
@@ -27,44 +27,46 @@ import math
 # defenders = [(d1x, d1y)]
 # # plot_game(grids1v1, value_function1v1, attackers, defenders)
 
-# # plot for 2v1 game
-# grid2v1 = Grid(np.array([-1.0, -1.0, -1.0, -1.0, -1.0, -1.0]), np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0]), 6, np.array([30, 30, 30, 30, 30, 30])) # original 45, on mars-14 20 is the upper bound
-# value2v1 = np.load('MRAG/2v1AttackDefend.npy')
-# print(f'The shape of the value function is {value2v1.shape} \n')
-# # define the joint states of (a1x, a1y, a2x, a2y, d1x, d1y)
-# a1x = 0.0
-# a1y = 0.0
-# a2x = 0.7
-# a2y = 0.2
-# d1x = -1.0
-# d1y = -1.0
-# jointstates2v1 = (a1x, a1y, a2x, a2y, d1x, d1y)
+# plot for 2v1 game
+grid2v1 = Grid(np.array([-1.0, -1.0, -1.0, -1.0, -1.0, -1.0]), np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0]),
+               6, np.array([30, 30, 30, 30, 30, 30])) # original 45, on mars-14 20 is the upper bound
+value2v1 = np.load('2v1AttackDefend.npy')
+print(f'The shape of the value function is {value2v1.shape} \n')
+# define the joint states of (a1x, a1y, a2x, a2y, d1x, d1y)
+a1x = -0.5
+a1y = 0.5
+a2x = -1.
+a2y = 0.
+d1x = 0.
+d1y = 0
+jointstates2v1 = (a1x, a1y, a2x, a2y, d1x, d1y)
 # attackers = [(a1x, a1y), (a2x, a2y)]
-# defenders = [(d1x, d1y)]
-# a1x_slice, a1y_slice, a2x_slice, a2y_slice, d1x_slice, d1y_slice = lo2slice2v1(jointstates2v1, slices=30)
-# # 
-# value_function2v1 = value2v1[:, :, a2x_slice, a2y_slice, d1x_slice, d1y_slice] 
-# print("Min value of the array {}".format(np.min(value_function2v1)))
-# print(f'The shape of the 2v1 value function is {value_function2v1.shape}. \n')
-# print(f'The HJ value of the current position {(a1x, a1y, a2x, a2y, d1x, d1y)} is {value2v1[a1x_slice, a1y_slice, a2x_slice, a2y_slice, d1x_slice, d1y_slice]}. \n')
-# # plot_2d(grid2v1, value_function2v1)
-# # plot_game(grid2v1, value_function2v1, attackers, defenders)
+attackers = [(a2x, a2y)]
+defenders = [(d1x, d1y)]
+a1x_slice, a1y_slice, a2x_slice, a2y_slice, d1x_slice, d1y_slice = lo2slice2v1(jointstates2v1, slices=30)
+#
+value_function2v1 = value2v1[:, :, a2x_slice, a2y_slice, d1x_slice, d1y_slice]
+print("Min value of the array {}".format(np.min(value_function2v1)))
+print(f'The shape of the 2v1 value function is {value_function2v1.shape}. \n')
+print(f'The HJ value of the current position {(a1x, a1y, a2x, a2y, d1x, d1y)} is {value2v1[a1x_slice, a1y_slice, a2x_slice, a2y_slice, d1x_slice, d1y_slice]}. \n')
+plot_2d(grid2v1, value_function2v1)
+plot_game(grid2v1, value_function2v1, attackers, defenders)
 
-# plot 1v0 reach-avoid game
-grids1v0 = Grid(np.array([-1.0, -1.0]), np.array([1.0, 1.0]), 2, np.array([100, 100])) # original 45
-value1v0 = np.load('MRAG/1v0AttackDefend.npy')
-print(f'The shape of the value function is {value1v0.shape} \n')
-# define the joint states of (a1x, a1y, d1x, d1y)
-a1x = 0
-a1y = 0
-jointstates1v0 = (a1x, a1y)
-a1x_slice, a1y_slice = lo2slice1v0(jointstates1v0, slices=100)
-# print(f'The attacker is at the location [{a1x}, {a1y}] and the defender is at the location [{d1x}, {d1y}] \n')
-print(f'The value function of the attacker at the location (0, 0) is {value1v0[a1x_slice, a1y_slice]}. \n')
-value_function1v0 = value1v0[:,:,0]  # , 0] if the saveAllTimeSteps=True. 0 is reachable set, -1 is target set
-print(f'The shape of the 1v1 value function is {value_function1v0.shape}. \n')
-# plot_2d(grids1v1, value_function1v1)
-# if want to add the positons of attackers and defenders
-attackers = [(a1x, a1y)]
-# plot_game(grids1v0, value_function1v0, attackers, defenders)
-plot_original(grids1v0, value_function1v0)
+# # plot 1v0 reach-avoid game
+# grids1v0 = Grid(np.array([-1.0, -1.0]), np.array([1.0, 1.0]), 2, np.array([100, 100])) # original 45
+# value1v0 = np.load('MRAG/1v0AttackDefend.npy')
+# print(f'The shape of the value function is {value1v0.shape} \n')
+# # define the joint states of (a1x, a1y, d1x, d1y)
+# a1x = 0
+# a1y = 0
+# jointstates1v0 = (a1x, a1y)
+# a1x_slice, a1y_slice = lo2slice1v0(jointstates1v0, slices=100)
+# # print(f'The attacker is at the location [{a1x}, {a1y}] and the defender is at the location [{d1x}, {d1y}] \n')
+# print(f'The value function of the attacker at the location (0, 0) is {value1v0[a1x_slice, a1y_slice]}. \n')
+# value_function1v0 = value1v0[:,:,0]  # , 0] if the saveAllTimeSteps=True. 0 is reachable set, -1 is target set
+# print(f'The shape of the 1v1 value function is {value_function1v0.shape}. \n')
+# # plot_2d(grids1v1, value_function1v1)
+# # if want to add the positons of attackers and defenders
+# attackers = [(a1x, a1y)]
+# # plot_game(grids1v0, value_function1v0, attackers, defenders)
+# plot_original(grids1v0, value_function1v0)
