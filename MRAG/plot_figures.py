b91653d83402931edba7825dab3bd2dd7006128b
hha160 <HuHY97@outlook.com>
2023-03-28 15:26:11 -0700

    feat: try new 2v1 value function in 6v2 game.

--- MRAG/plot_figures.py
+++ MRAG/plot_figures.py
@@ -17,8 +17,8 @@ print(f'The shape of the 2v1 value function is {value2v1.shape} \n')
 
 # 2v1 simulation
 # initial positions
-attackers = [(-0.01026, 0.07432), (0.3827, 0.4783)]  # [(-0.5, 0.0), (0.0, 0.8)], [(-0.5, 0.5), (-0.3, -0.8)]
-defenders = [(0.08876, 0.07432)] # [(0.3, -0.3)], [(0.0, 0.0)]
+attackers = [(-0.2, 0.0), (-0.8, -0.8)]  # [(-0.5, 0.0), (0.0, 0.8)], [(-0.5, 0.5), (-0.3, -0.8)]
+defenders = [(0.0, 0.0)] # [(0.3, -0.3)], [(0.0, 0.0)]
 a1x = attackers[0][0]
 a1y = attackers[0][1]
 a2x = attackers[1][0]
@@ -42,7 +42,7 @@ defenders_plot1 = [(d1x, d1y)]
 jointstates2v1 = (a1x, a1y, a2x, a2y, d1x, d1y)
 a1x_slice, a1y_slice, a2x_slice, a2y_slice, d1x_slice, d1y_slice = lo2slice2v1(jointstates2v1, slices=30)
 # value_function2v1 = value2v1[:, :, a2x_slice, a2y_slice, d1x_slice, d1y_slice]
-value_function2v1 = value2v1[:, :, a2x_slice, a2y_slice, d1x_slice, d1y_slice]
+value_function2v1 = value2v1[a1x_slice, a1y_slice, :, :, d1x_slice, d1y_slice]
 
 # plotting players
 attackers_plot2 = [(a1x, a1y), (a2x, a2y)]
