f7868f14713d45daceaae846586bf92b26bdb125
hha160 <HuHY97@outlook.com>
2023-05-03 12:27:04 -0700

    feat: revise the figures

--- MRAG/find_tEarlist.py
+++ MRAG/find_tEarlist.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # neg -> pos, pos -> neg
 def find_sign_change(grid, V, x):
-    indexAtX = grid.get_index(x) # 这里师兄默认是2-d的系统
+    indexAtX = grid.get_index(x) # default 2d system
     print("index at x {} ".format(indexAtX))
     valueAtX = V[indexAtX[0],indexAtX[1], :]  # [V[this position at RAS], V[this position at RAS-tstep], ..., V[this position at tau=0]]
     print("valueAtX is {}".format(valueAtX))
@@ -10,8 +10,8 @@ def find_sign_change(grid, V, x):
     valueIsNeg = (valueAtX<=0).astype(int) # neg = 1, not neg = 0
     print(valueIsNeg)
 
-    checkList = valueIsNeg - np.append(valueIsNeg[1:],valueIsNeg[-1]) # 左移1位
+    checkList = valueIsNeg - np.append(valueIsNeg[1:],valueIsNeg[-1]) # move left 1 position
     # neg -> pos, pos -> neg
     # neg(True) - pos(False) = 1 --> neg to pos
     # pos(False) - neg(True) = -1 --> pos to neg
-    return np.where(checkList==1)[0], np.where(checkList==-1)[0]  # 返回的是正负边缘的tau的时间点
+    return np.where(checkList==1)[0], np.where(checkList==-1)[0]  # return the time point of tau with positive and negative edges
