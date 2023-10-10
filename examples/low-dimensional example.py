60561089e0d38fa550be392e7dc379ce8d8b5125
ChongChongbst <91517025+ChongChongbst@users.noreply.github.com>
2023-10-10 13:09:31 -0700

    new plot setting

--- examples/low-dimensional example.py
+++ examples/low-dimensional example.py
@@ -47,7 +47,7 @@ sys = Plane2D()
 
 # STEP 5: Initialize plotting option
 po2 = PlotOptions(do_plot=True, plot_type="2d_plot", plotDims=[0,1],
-                  slicesCut=[])
+                  slicesCut=[], colorscale="Hot")
 
 # STEP 6: Call HJSolver function
 compMethod = { "TargetSetMode": "None"}
@@ -55,34 +55,36 @@ result_2 = HJSolver(sys, g, Initial_value_f, tau, compMethod, po2)
 
 
 
-##################################################### 1D EXAMPLE #####################################################
-# STEP 1: Define grid
-grid_min = np.array([-4.0])
-grid_max = np.array([4.0])
-dims = 1
-N = np.array([40])
-g = Grid(grid_min, grid_max, dims, N)
+# ##################################################### 1D EXAMPLE #####################################################
+# # STEP 1: Define grid
+# grid_min_1 = np.array([-4.0])
+# grid_max_1 = np.array([4.0])
+# dims_1 = 1
+# N_1 = np.array([40])
+# g_1 = Grid(grid_min_1, grid_max_1, dims_1, N_1)
 
-# STEP 2: Generate initial values for grid using shape functions
-target_min = np.array([-1.0])
-target_max = np.array([1.0])
-Initial_value_f = ShapeRectangle(g, target_min, target_max)
+# # STEP 2: Generate initial values for grid using shape functions
+# target_min_1 = np.array([-1.0])
+# target_max_1 = np.array([1.0])
+# Initial_value_f_1 = ShapeRectangle(g_1, target_min_1, target_max_1)
 
-# STEP 3: Time length for computations
-Lookback_length = 2.0
-t_step = 0.05
+# # STEP 3: Time length for computations
+# Lookback_length = 2.0
+# t_step = 0.05
 
-small_number = 1e-5
-tau = np.arange(start=0, stop=Lookback_length + small_number, step=t_step)
+# small_number = 1e-5
+# tau = np.arange(start=0, stop=Lookback_length + small_number, step=t_step)
 
-# STEP 4: System dynamics for computation
-sys = Plane1D()
+# # STEP 4: System dynamics for computation
+# sys_1 = Plane1D()
 
-# STEP 5: Initialize plotting option
-po2 = PlotOptions(do_plot=True, plot_type="1d_plot", plotDims=[0],
-                  slicesCut=[])
+# # STEP 5: Initialize plotting option
+# po2 = PlotOptions(do_plot=True, plot_type="1d_plot", plotDims=[0],
+#                   slicesCut=[])
 
-# STEP 6: Call HJSolver function
-compMethod = { "TargetSetMode": "None"}
-result_1 = HJSolver(sys, g, Initial_value_f, tau, compMethod, po2)
+# # STEP 6: Call HJSolver function
+# compMethod = { "TargetSetMode": "None"}
+
+# print(1)
+# result_1 = HJSolver(sys_1, g_1, Initial_value_f_1, tau, compMethod, po2)
 
