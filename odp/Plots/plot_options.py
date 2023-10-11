9f8b4a480e223f161950bd5f6d53419120029770
ChongChongbst <91517025+ChongChongbst@users.noreply.github.com>
2023-10-11 09:17:33 -0700

    Update plot_options.py

--- odp/Plots/plot_options.py
+++ odp/Plots/plot_options.py
@@ -16,10 +16,10 @@ class PlotOptions:
     if len(plotDims) != 1 and len(plotDims) != 2 and len(plotDims) != 3:
         raise Exception("Make sure that dim_plot size is 1, 2, or 3!!")
     
-    if plot_type == "value" and len(plotDims) != 3:
+    if plot_type == "value" and len(plotDims) == 3:
         raise Exception("Make sure that dim_plot size is 1 or 2 for value function plot!!")
     
-    if plot_type == "set" and len(plotDims) != 1:
+    if plot_type == "set" and len(plotDims) == 1:
         raise Exception("Make sure that dim_plot size is 2 or 3 for 0 sublevel set plot!!")
 
     self.do_plot = do_plot
