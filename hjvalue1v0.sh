5c35b9ea8e0d4dd741164560b8babd617534f3f5
hha160 <HuHY97@outlook.com>
2023-11-30 11:34:34 -0800

    add sh file to run on the cluster

---
+++ hjvalue1v0.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1         # Number of GPUs (per node)
+#SBATCH --mem=64G               # memory (per node)
+#SBATCH --time=1-15:00            # time (DD-HH:MM)
+#SBATCH --cpus-per-task=6         # Number of CPUs (per task)
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hha160@sfu.ca
+#SBATCH --nodelist=cs-venus-06
+#SBATCH --partition=long
+#SBATCH --output=/localscratch/hha160/MARAG_Output/hjvalue1v0.out
+#SBATCH -J hjvalue1v0
+
+echo 'Start to compile the sh file now!'
+
+source ~/.zshrc
+conda activate odp
+git checkout solar
+cd /localscratch/hha160/MARAG
+echo 'Start to run the main code now!'
+python MRAG/hjvale1v0.py
