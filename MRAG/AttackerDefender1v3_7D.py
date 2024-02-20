2c656dcd9dd7298e4a129a4456d6a61951e880aa
hha160 <HuHY97@outlook.com>
2024-02-20 12:19:06 -0800

    add 1vs3 7D game

---
+++ MRAG/AttackerDefender1v3_7D.py
@@ -0,0 +1,314 @@
+import heterocl as hcl
+import math
+import numpy as np
+
+""" 7D 1 vs. 3 AttackerDefender DYNAMICS IMPLEMENTATION 
+ xA11_dot = vA * u11
+ xA12_dot = vA * u12
+ xD11_dot = vD * d11
+ xD12_dot = vD * d12
+ xD21_dot = vD * d21
+ xD22_dot = vD * d22
+ xD3_dot = vD * d3
+ """
+
+
+class AttackerDefender1v3_7D:
+    def __init__(self, x=[0, 0, 0, 0, 0, 0, 0], uMin=-1, uMax=1, dMin=-1,
+                 dMax=1, uMode="min", dMode="max", speed_a=1.0, speed_d=1.5):
+        """Creates 1 Attacker and 3 Defenders with 1 defender could only move in 1 direction
+           The controls are the control inputs of the Attackers.
+           The disturbances are the control inputs of the Defender.
+        Args:
+            x (list, optional): Initial state . Defaults to [0, 0, 0, 0, 0, 0, 0].
+            uMin (list, optional): Lowerbound of user control. Defaults to [-1,-1].
+            uMax (list, optional): Upperbound of user control. Defaults to [1,1].
+            dMin (list, optional): Lowerbound of disturbance to user control, . Defaults to [-0.25,-0.25].
+            dMax (list, optional): Upperbound of disturbance to user control. Defaults to [0.25,0.25].
+            uMode (str, optional): Accepts either "min" or "max".
+                                   * "min" : have optimal control reach goal
+                                   * "max" : have optimal control avoid goal
+                                   Defaults to "min".
+            dMode (str, optional): Accepts whether "min" or "max" and should be opposite of uMode.
+                                   Defaults to "max". reach-avoid game is a min(u) max(d) game
+            speed_a (int, optional): The maximum speed of the attacker. Defaults to 1.0.
+            speed_d (int, optional): The maximum speed of the defender. Defaults to 1.0.
+        """
+        self.x = x
+        self.uMax = uMax
+        self.uMin = uMin
+        self.dMax = dMax
+        self.dMin = dMin
+        assert (uMode in ["min", "max"])
+        self.uMode = uMode
+        # if uMode == "min":
+        #     assert (dMode == "max")
+        # else:
+        #     assert (dMode == "min")
+        self.dMode = dMode
+        # maximum speed of attackers and defenders
+        self.speed_a = speed_a
+        self.speed_d = speed_d
+
+    def dynamics(self, t, state, uOpt, dOpt):
+        # maximum velocity
+
+        xA11_dot = hcl.scalar(0, "xA11_dot")
+        xA12_dot = hcl.scalar(0, "xA12_dot")
+        xD11_dot = hcl.scalar(0, "xD11_dot")
+        xD12_dot = hcl.scalar(0, "xD12_dot")
+        xD21_dot = hcl.scalar(0, "xD21_dot")
+        xD22_dot = hcl.scalar(0, "xD22_dot")
+        xD3_dot = hcl.scalar(0, "xD3_dot")
+
+
+        xA11_dot[0] = self.speed_a * uOpt[0]
+        xA12_dot[0] = self.speed_a * uOpt[1]
+        xD11_dot[0] = self.speed_d * dOpt[0]
+        xD12_dot[0] = self.speed_d * dOpt[1]
+        xD21_dot[0] = self.speed_d * dOpt[2]
+        xD22_dot[0] = self.speed_d * dOpt[3]
+        xD3_dot[0] = self.speed_d * dOpt[4]
+
+        return xA11_dot[0], xA12_dot[0], xD11_dot[0], xD12_dot[0], xD21_dot[0], xD22_dot[0], xD3_dot[0]
+
+    def opt_ctrl(self, t, state, spat_deriv):
+        """
+        :param t: time t
+        :param state: tuple of coordinates
+        :param spat_deriv: tuple of spatial derivative in all dimensions
+        :return:
+        """
+        # In 1v3 AttackerDefender, a(t) = [a1, a2]^T
+        opt_a1 = hcl.scalar(0, "opt_a1")
+        opt_a2 = hcl.scalar(0, "opt_a2")      
+        # Just create and pass back, even though they're not used
+        in3 = hcl.scalar(0, "in3")
+        in4 = hcl.scalar(0, "in4")
+        in5 = hcl.scalar(0, "in5")
+        in6 = hcl.scalar(0, "in6")
+        in7 = hcl.scalar(0, "in7")
+        # declare the hcl scalars for relevant spat_derivs
+        deriv1 = hcl.scalar(0, "deriv1")
+        deriv2 = hcl.scalar(0, "deriv2")
+
+        deriv1[0] = spat_deriv[0]
+        deriv2[0] = spat_deriv[1]
+
+        ctrl_len1 = hcl.sqrt(deriv1[0] * deriv1[0] + deriv2[0] * deriv2[0])     
+
+        if self.uMode == "min":
+            with hcl.if_(ctrl_len1 == 0):
+                opt_a1[0] = 0.0
+                opt_a2[0] = 0.0
+            with hcl.else_():
+                opt_a1[0] = -deriv1[0] / ctrl_len1
+                opt_a2[0] = -deriv2[0] / ctrl_len1
+
+        else:
+            with hcl.if_(ctrl_len1 == 0):
+                opt_a1[0] = 0.0
+                opt_a2[0] = 0.0
+            with hcl.else_():
+                opt_a1[0] = deriv1[0] / ctrl_len1
+                opt_a2[0] = deriv2[0] / ctrl_len1
+            
+        return opt_a1[0], opt_a2[0], in3[0], in4[0], in5[0], in6[0], in7[0]
+
+    def opt_dstb(self, t, state, spat_deriv):
+        """
+        :param spat_deriv: tuple of spatial derivative in all dimensions
+        :return: a tuple of optimal disturbances
+        """
+        # Graph takes in 4 possible inputs, by default, for now
+        d1 = hcl.scalar(0, "d1")
+        d2 = hcl.scalar(0, "d2")
+        d3 = hcl.scalar(0, "d3")
+        d4 = hcl.scalar(0, "d4")
+        d5 = hcl.scalar(0, "d5")
+        # Just create and pass back, even though they're not used
+        d6 = hcl.scalar(0, "d6")
+        d7 = hcl.scalar(0, "d7")
+
+
+        deriv1 = hcl.scalar(0, "deriv1")
+        deriv2 = hcl.scalar(0, "deriv2")
+        deriv3 = hcl.scalar(0, "deriv3")
+        deriv4 = hcl.scalar(0, "deriv4")
+        deriv5 = hcl.scalar(0, "deriv5")
+
+        deriv1[0] = spat_deriv[2]
+        deriv2[0] = spat_deriv[3]
+        deriv3[0] = spat_deriv[4]
+        deriv4[0] = spat_deriv[5]
+        deriv5[0] = spat_deriv[6]
+
+        dstb_len1 = hcl.sqrt(deriv1[0] * deriv1[0] + deriv2[0] * deriv2[0])
+        dstb_len2 = hcl.sqrt(deriv3[0] * deriv3[0] + deriv4[0] * deriv4[0])
+        dstb_len3 = hcl.sqrt(deriv5[0] * deriv5[0])
+
+        # with hcl.if_(self.dMode == "max"):
+        if self.dMode == 'max':
+            with hcl.if_(dstb_len1 == 0):
+                d1[0] = 0.0
+                d2[0] = 0.0
+            with hcl.else_():
+                d1[0] = deriv1[0] / dstb_len1
+                d2[0] = deriv2[0] / dstb_len1
+            
+            with hcl.if_(dstb_len2 == 0):
+                d3[0] = 0.0
+                d4[0] = 0.0
+            with hcl.else_():
+                d3[0] = deriv3[0] / dstb_len2
+                d4[0] = deriv4[0] / dstb_len2
+
+            with hcl.if_(dstb_len3 == 0):
+                d5[0] = 0.0
+            with hcl.else_():
+                d5[0] = deriv5[0] / dstb_len3
+
+        else:
+            with hcl.if_(dstb_len1 == 0):
+                d1[0] = 0.0
+                d2[0] = 0.0
+            with hcl.else_():
+                d1[0] = -deriv1[0]/ dstb_len1
+                d2[0] = -deriv2[0] / dstb_len1
+            
+            with hcl.if_(dstb_len2 == 0):
+                d3[0] = 0.0
+                d4[0] = 0.0
+            with hcl.else_():
+                d3[0] = -deriv3[0]/ dstb_len2
+                d4[0] = -deriv4[0] / dstb_len2
+            
+            with hcl.if_(dstb_len3 == 0):
+                d5[0] = 0.0
+            with hcl.else_():
+                d5[0] = -deriv5[0]/ dstb_len3
+
+        return d1[0], d2[0], d3[0], d4[0], d5[0], d6[0], d7[0]
+
+        # The below function can have whatever form or parameters users want
+        # These functions are not used in HeteroCL program, hence is pure Python code and
+        # can be used after the value function has been obtained.
+
+    def optCtrl_inPython(self, spat_deriv):
+        """
+        :param spat_deriv: tuple of spatial derivative in all dimensions
+        :return: a tuple of optimal control of two attackers
+        """
+        opt_a1 = self.uMax
+        opt_a2 = self.uMax
+        deriv1 = spat_deriv[0]
+        deriv2 = spat_deriv[1]
+        ctrl_len1 = np.sqrt(deriv1*deriv1 + deriv2*deriv2)
+
+        # The initialized control only change sign in the following cases
+        if self.uMode == "min":
+            if ctrl_len1 == 0:
+                opt_a1 = 0.0
+                opt_a2 = 0.0
+            else:
+                opt_a1 = -deriv1 / ctrl_len1
+                opt_a2 = -deriv2 / ctrl_len1
+           
+        else:
+            if ctrl_len1 == 0:
+                opt_a1 = 0.0
+                opt_a2 = 0.0
+            else:
+                opt_a1 = deriv1 / ctrl_len1
+                opt_a2 = deriv2 / ctrl_len1
+            
+        return (opt_a1, opt_a2)
+    
+    def optDstb_inPython(self, spat_deriv):
+        """
+        :param spat_deriv: tuple of spatial derivative in all dimensions
+        :return: a tuple of optimal control of the defender (disturbances)
+        """
+        opt_d1 = self.dMax
+        opt_d2 = self.dMax
+        opt_d3 = self.dMax
+        opt_d4 = self.dMax
+        opt_d5 = self.dMax
+
+        deriv3 = spat_deriv[2]
+        deriv4 = spat_deriv[3]
+        deriv5 = spat_deriv[4]
+        deriv6 = spat_deriv[5]
+        deriv7 = spat_deriv[6]
+
+        dstb_len1 = np.sqrt(deriv3*deriv3 + deriv4*deriv4)
+        dstb_len2 = np.sqrt(deriv5*deriv5 + deriv6*deriv6)
+        dstb_len3 = np.sqrt(deriv7*deriv7)
+
+        # The initialized control only change sign in the following cases
+        if self.dMode == "max":
+            if dstb_len1 == 0:
+                opt_d1 = 0.0
+                opt_d2 = 0.0
+            else:
+                opt_d1 = self.speed_d*deriv3 / dstb_len1
+                opt_d2 = self.speed_d*deriv4 / dstb_len1
+            
+            if dstb_len2 == 0:
+                opt_d3 = 0.0
+                opt_d4 = 0.0
+            else:
+                opt_d3 = self.speed_d*deriv5 / dstb_len2
+                opt_d4 = self.speed_d*deriv6 / dstb_len2
+            
+            if dstb_len3 == 0:
+                opt_d5 = 0.0
+            else:
+                opt_d5 = self.speed_d*deriv7 / dstb_len3
+
+        else:
+            if dstb_len1 == 0:
+                opt_d1 = 0.0
+                opt_d2 = 0.0
+            else:
+                opt_d1 = -self.speed_d*deriv3 / dstb_len1
+                opt_d2 = -self.speed_d*deriv4 / dstb_len1
+            
+            if dstb_len2 == 0:
+                opt_d3 = 0.0
+                opt_d4 = 0.0
+            else:
+                opt_d3 = -self.speed_d*deriv5 / dstb_len2
+                opt_d4 = -self.speed_d*deriv6 / dstb_len2
+            
+            if dstb_len3 == 0:
+                opt_d5 = 0.0
+            else:
+                opt_d5 = -self.speed_d*deriv7 / dstb_len3
+
+        return (opt_d1, opt_d2, opt_d3, opt_d4, opt_d5)
+
+    def capture_set1(self, grid, capture_radius, mode):
+        # The capture relationship between the A and the D1
+        data = np.power(grid.vs[0] - grid.vs[2], 2) + np.power(grid.vs[1] -grid.vs[3], 2)
+        if mode == "capture":
+            return np.sqrt(data) - capture_radius
+        if mode == "escape":
+            return capture_radius - np.sqrt(data)
+
+    def capture_set2(self, grid, capture_radius, mode):
+        # The capture relationship between the A and the D2
+        data = np.power(grid.vs[0] - grid.vs[4], 2) + np.power(grid.vs[1] -grid.vs[5], 2)
+        if mode == "capture":
+            return np.sqrt(data) - capture_radius
+        if mode == "escape":
+            return capture_radius - np.sqrt(data)
+    
+    def capture_set3(self, grid, capture_radius, mode):
+        # The capture relationship between the A and the D3
+        #TODO: since the D3 could only move in one direction, the y position of D3 should be fixed as a constant
+        data = np.power(grid.vs[0] - grid.vs[6], 2) + np.power(grid.vs[1] - 0, 2)
+        if mode == "capture":
+            return np.sqrt(data) - capture_radius
+        if mode == "escape":
+            return capture_radius - np.sqrt(data)
---
+++ MRAG/hjvalue1v3_7D_solar.py
@@ -0,0 +1,117 @@
+import numpy as np
+# Utility functions to initialize the problem
+from odp.Grid import Grid
+from odp.Shapes import *
+# Specify the  file that includes dynamic systems, AttackerDefender4D
+from AttackerDefender1v3_7D import *
+# Plot options
+from odp.Plots import PlotOptions
+from odp.Plots.plotting_utilities import plot_2d, plot_isosurface
+# Solver core
+from odp.solver import HJSolver, computeSpatDerivArray
+import math
+import time
+import gc
+import os, psutil
+
+
+""" USER INTERFACES
+- 1. Initialize the grids
+- 2. Initialize the dynamics
+- 3. Instruct the avoid set and reach set
+- 4. Set the look-back length and time step
+- 5. Call HJSolver function
+- 6. Save the value function
+"""
+
+##################################################### EXAMPLE 4 2v1AttackerDefender ####################################
+# Record the time of whole process
+start_time = time.time()
+print("The start time is {}".format(start_time))
+
+# 1. Initialize the grids
+grid_size = 10
+
+grids = Grid(np.array([-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0]), np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]), 
+             7, np.array([grid_size, grid_size, grid_size, grid_size, grid_size, grid_size, grid_size])) 
+
+
+process = psutil.Process(os.getpid())
+print("1. Gigabytes consumed by the grids is {}".format(process.memory_info().rss/(1e9)))  # in bytes
+
+# 2. Initialize the dynamics
+agents_1v3_7D = AttackerDefender1v3_7D(uMode="min", dMode="max")  # 1 vs.3 with 1 defender restricted (7 dim dynamics)
+
+# 3. Instruct the avoid set and reach set
+# 3.1 Avoid set, no constraint means inf
+capture_a1d1 = agents_1v3_7D.capture_set1(grids, 0.1, "capture")  # a1 is captured by d1
+capture_a1d1 = np.array(capture_a1d1, dtype='float32')
+
+capture_a1d2 = agents_1v3_7D.capture_set2(grids, 0.1, "capture")  # a1 is captured by d2
+capture_a1d2 = np.array(capture_a1d2, dtype='float32')
+
+capture_a1d3 = agents_1v3_7D.capture_set3(grids, 0.1, "capture")  # a1 is captured by d3
+capture_a1d3 = np.array(capture_a1d3, dtype='float32')
+
+avoid_set = np.minimum(capture_a1d1, np.minimum(capture_a1d2, capture_a1d3))
+avoid_set = np.array(avoid_set, dtype='float32')
+
+del capture_a1d1
+del capture_a1d2
+del capture_a1d3
+
+process = psutil.Process(os.getpid())
+print("2. Gigabytes consumed of the avoid set is {}".format(process.memory_info().rss/(1e9)))  # in bytes
+gc.collect()
+
+# 3.2 Reach set
+goal1_destination = ShapeRectangle(grids, [0.6, 0.1, -1000, -1000, -1000, -1000, -1000], [0.8, 0.3, 1000, 1000, 1000, 1000, 1000])  # attacker arrives target
+goal2_escape_d1 = agents_1v3_7D.capture_set1(grids, 0.1, "escape")  # a1 escape from d1
+goal3_escape_d2 = agents_1v3_7D.capture_set2(grids, 0.1, "escape")  # a1 escape from d2
+goal4_escape_d3 = agents_1v3_7D.capture_set3(grids, 0.1, "escape")  # a1 escape from d3
+
+reach_set = np.maximum(goal1_destination, np.maximum(goal2_escape_d1, np.maximum(goal3_escape_d2, goal4_escape_d3)))
+reach_set = np.array(reach_set, dtype='float32')
+
+process = psutil.Process(os.getpid())
+print("3. Gigabytes consumed of the reach set is {}".format(process.memory_info().rss/(1e9)))  # in bytes
+gc.collect()
+
+
+# 4. Set the look-back length and time step
+#TODO: also compare the lookback length with the same grid size
+lookback_length = 1.0  # try 1.5, 2.0, 2.5, 3.0, 5.0, 6.0, 8.0  
+t_step = 0.025
+
+# Actual calculation process, needs to add new plot function to draw a 2D figure
+small_number = 1e-5
+tau = np.arange(start=0, stop=lookback_length + small_number, step=t_step)
+
+# while plotting make sure the len(slicesCut) + len(plotDims) = grid.dims
+po = PlotOptions(do_plot=False, plot_type="2d_plot", plotDims=[0, 1], slicesCut=[22, 22])
+
+# 5. Call HJSolver function
+compMethods = {"TargetSetMode": "minVWithVTarget", "ObstacleSetMode": "maxVWithObstacle"} # original one
+solve_start_time = time.time()
+result = HJSolver(agents_1v3_7D, grids, [reach_set, avoid_set], tau, compMethods, po, saveAllTimeSteps=False) # original one
+
+process = psutil.Process(os.getpid())
+print(f"The CPU memory used during the calculation of the value function is {process.memory_info().rss/(1e9): .2f} GB.")  # in bytes
+
+solve_end_time = time.time()
+print(f'The shape of the value function is {result.shape} \n')
+print(f"The size of the value function is {result.nbytes / (1e9): .2f} GB or {result.nbytes/(1e6)} MB.")
+print(f"The time of solving HJ is {solve_end_time - solve_start_time} seconds.")
+print(f'The shape of the value function is {result.shape} \n')
+
+print("The calculation is done! \n")
+
+# 6. Save the value function
+np.save(f'1v3_7DAttackDefend_g{grid_size}_T{lookback_length}_speed15.npy', result)
+
+print(f"The value function has been saved successfully.")
+
+# Record the time of whole process
+end_time = time.time()
+print(f"The end time is {end_time}")
+print(f"The time of whole process is {end_time - start_time} seconds.")
