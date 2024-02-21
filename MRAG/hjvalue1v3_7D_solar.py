506cb16d0f16ffeb7056ed4c38fb9692cbfe1e59
hha160 <HuHY97@outlook.com>
2024-02-21 14:13:52 -0800

    try 1v3 7D with grid=18 and T=4.5

--- MRAG/hjvalue1v3_7D_solar.py
+++ MRAG/hjvalue1v3_7D_solar.py
@@ -30,7 +30,7 @@ start_time = time.time()
 print("The start time is {}".format(start_time))
 
 # 1. Initialize the grids
-grid_size = 20
+grid_size = 18
 
 grids = Grid(np.array([-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0]), np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]), 
              7, np.array([grid_size, grid_size, grid_size, grid_size, grid_size, grid_size, grid_size])) 
