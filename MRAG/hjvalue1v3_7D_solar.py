4a9e27f73b24705de05eb1f78ea502895e153608
hha160 <HuHY97@outlook.com>
2024-02-29 13:57:20 -0800

    try 7D with grid=19 using revised solver

--- MRAG/hjvalue1v3_7D_solar.py
+++ MRAG/hjvalue1v3_7D_solar.py
@@ -30,7 +30,7 @@ start_time = time.time()
 print("The start time is {}".format(start_time))
 
 # 1. Initialize the grids
-grid_size = 18
+grid_size = 19
 
 grids = Grid(np.array([-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0]), np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]), 
              7, np.array([grid_size, grid_size, grid_size, grid_size, grid_size, grid_size, grid_size])) 
