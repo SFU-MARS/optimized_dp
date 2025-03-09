a1ed6286c80014cee9b4d8977b7c697c1b3c8ace
mochen72 <mochen@cs.sfu.ca>
2025-03-09 06:40:23 +0000

    minor improvement in user friendliness for grid class

--- odp/Grid/GridProcessing.py
+++ odp/Grid/GridProcessing.py
@@ -1,11 +1,18 @@
 import numpy as np
 import math
+from typing import List
 
 
 class Grid:
-    def __init__(self, minBounds, maxBounds, dims, pts_each_dim, periodicDims=[]):
-        """ 
-
+    def __init__(
+        self,
+        minBounds: List,
+        maxBounds: List,
+        dims: int,
+        pts_each_dim: List,
+        periodicDims: List = [],
+    ):
+        """
         Args:
             minBounds (list): The lower bounds of each dimension in the grid
             maxBounds (list): The upper bounds of each dimension in the grid
@@ -13,18 +20,20 @@ class Grid:
             pts_each_dim (list): The number of points for each dimension in the grid
             periodicDim (list, optional): A list of periodic dimentions (0-indexed). Defaults to [].
         """
-        self.max = maxBounds
-        self.min = minBounds
-        self.dims = len(pts_each_dim)
-        self.pts_each_dim = pts_each_dim
+        assert len(minBounds) == len(maxBounds) == len(pts_each_dim) == dims
+
+        self.max = np.array(maxBounds)
+        self.min = np.array(minBounds)
+        self.dims = dims
+        self.pts_each_dim = np.array(pts_each_dim)
         self.pDim = periodicDims
 
-        # Exclude the upper bounds for periodic dimensions is not included 
+        # Exclude the upper bounds for periodic dimensions is not included
         # e.g. [-pi, pi)
         for dim in self.pDim:
-            self.max[dim] = self.min[dim] + \
-                (self.max[dim] - self.min[dim]) * \
-                (1 - 1/self.pts_each_dim[dim])
+            self.max[dim] = self.min[dim] + (self.max[dim] - self.min[dim]) * (
+                1 - 1 / self.pts_each_dim[dim]
+            )
         self.dx = (self.max - self.min) / (self.pts_each_dim - 1.0)
 
         """
@@ -38,8 +47,7 @@ class Grid:
         """
         self.grid_points = []
         for i in range(dims):
-            tmp = np.linspace(self.min[i], self.max[i],
-                              num=self.pts_each_dim[i])
+            tmp = np.linspace(self.min[i], self.max[i], num=self.pts_each_dim[i])
             broadcast_map = np.ones(self.dims, dtype=int)
             broadcast_map[i] = self.pts_each_dim[i]
             self.grid_points.append(tmp)
@@ -50,8 +58,18 @@ class Grid:
             tmp = np.reshape(tmp, tuple(broadcast_map))
             self.vs.append(tmp)
 
+    def __str__(self):
+        return (
+            f"Grid:\n"
+            + f"  max: {self.max}\n"
+            + f"  min: {self.min}\n"
+            + f"  pts_each_dim: {self.pts_each_dim}\n"
+            + f"  pDim: {self.pDim}\n"
+            + f"  dx: {self.dx}\n"
+        )
+
     def get_index(self, state):
-        """ Returns a tuple of the closest index of each state in the grid
+        """Returns a tuple of the closest index of each state in the grid
 
         Args:
             state (tuple): state of dynamic object
@@ -77,7 +95,7 @@ class Grid:
         Assumes that the state is within the bounds of the grid
 
         Args:
-            V (np.array): value function of solved HJ PDE 
+            V (np.array): value function of solved HJ PDE
             state (tuple): state of dynamic object
 
         Returns:
