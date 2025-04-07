7f9ac9d0123d38fa688ef968e836d087f2848eb3
Oren Bell <virtualoren@gmail.com>
2025-04-07 05:13:51 -0500

    Removed references to now missing Humannoid6D

--- odp/dynamics/__init__.py
+++ odp/dynamics/__init__.py
@@ -3,7 +3,6 @@ from odp.dynamics.DubinsCar import DubinsCar
 from odp.dynamics.DubinsCar4D import DubinsCar4D
 from odp.dynamics.DubinsCar4D2 import DubinsCar4D2
 from odp.dynamics.DubinsCar5DAvoid import DubinsCar5DAvoid
-from odp.dynamics.Humannoid6D_sys1 import Humanoid_6D
 from odp.dynamics.ROV_WaveBwds_6D import ROV_WaveBwds_6D
 from odp.dynamics.Plane2D import Plane2D
 from odp.dynamics.Plane1D import Plane1D
