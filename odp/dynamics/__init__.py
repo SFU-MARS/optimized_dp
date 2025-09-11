9bf3238b2cc2eee5af93762770f560471b60cbab
hha160 <hha160@cs-mars-14.cmpt.sfu.ca>
2025-09-11 11:51:02 -0700

    change the sap_derive in compute_trajectory_TTR

--- odp/dynamics/__init__.py
+++ odp/dynamics/__init__.py
@@ -1,11 +1,11 @@
 from odp.dynamics.DubinsCapture import DubinsCapture
 from odp.dynamics.DubinsCar import DubinsCar
 from odp.dynamics.DubinsCar4D import DubinsCar4D
-from odp.dynamics.DubinsCar5D import DubinsCar5D
+# from odp.dynamics.DubinsCar5D import DubinsCar5D
 from odp.dynamics.DubinsCar4D2 import DubinsCar4D2
 from odp.dynamics.DubinsCar5DAvoid import DubinsCar5DAvoid
 from odp.dynamics.ROV_WaveBwds_6D import ROV_WaveBwds_6D
 from odp.dynamics.Plane2D import Plane2D
 from odp.dynamics.Plane1D import Plane1D
 from odp.dynamics.DubinsAirplane6D import DubinsAirplane6D
-from odp.dynamics.PlaneQuad6D import PlaneQuad6D
+# from odp.dynamics.PlaneQuad6D import PlaneQuad6D
